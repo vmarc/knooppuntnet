15a53231c0e0a413a4a7c86d1176c30c285d90ea
vmarc <marc@vmarc.be>
2018-09-10 09:51:08 +0200

    Issue #23 Hiking network Brabantse Wal no longer sorted before ' and A

--- production/core/src/main/scala/kpn/core/facade/pages/SubsetNetworksPageBuilderImpl.scala
+++ production/core/src/main/scala/kpn/core/facade/pages/SubsetNetworksPageBuilderImpl.scala
@@ -5,7 +5,6 @@ import kpn.core.repository.NetworkRepository
 import kpn.core.repository.OverviewRepository
 import kpn.core.util.Formatter.number
 import kpn.core.util.Formatter.percentage
-import kpn.shared.LatLonImpl
 import kpn.shared.Subset
 import kpn.shared.subset.SubsetNetworksPage
 
@@ -20,21 +19,11 @@ class SubsetNetworksPageBuilderImpl(
     val subsetInfo = SubsetInfoBuilder.newSubsetInfo(subset, figures)
     val networks = networkRepository.networks(subset, Couch.uiTimeout)
 
-    // bring network "Brabantse Wal" to the front of the list
-    val sortedNetworks = networks.filter(_.id == 3138543) ++ networks.filterNot(_.id == 3138543)
-
     val routeCount = networks.map(_.routeCount).sum
     val brokenRouteNetworkCount = networks.count(_.brokenRouteCount > 0)
     val brokenRouteNetworkPercentage = percentage(brokenRouteNetworkCount, networks.size)
     val brokenRouteCount = networks.map(_.brokenRouteCount).sum
     val brokenRoutePercentage = percentage(brokenRouteCount, routeCount)
-
-    val networksWithFixedCenter = sortedNetworks.map { network => // TODO remove this temporary fix once database has been regenerated
-      network.center match {
-        case Some(LatLonImpl("NaN", "NaN")) => network.copy(center = None)
-        case _ => network
-      }
-    }
 
     SubsetNetworksPage(
       subsetInfo,
@@ -48,7 +37,7 @@ class SubsetNetworksPageBuilderImpl(
       brokenRoutePercentage = brokenRoutePercentage,
       unaccessibleRouteCount = networks.map(_.unaccessibleRouteCount).sum,
       analysisUpdatedTime = "TODO",
-      networks = networksWithFixedCenter
+      networks = networks
     )
   }
 }
