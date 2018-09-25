67a080ce9d223ffdbafe217e16cbf207ff8cfc8f
vmarc <marc@vmarc.be>
2018-09-25 14:30:10 +0200

    wip - node integrity check improvements

---
+++ production/core/src/main/scala/kpn/core/tools/investigation/NetworksWithNodesWithRoleConnection.scala
@@ -0,0 +1,16 @@
+package kpn.core.tools.investigation
+
+import kpn.core.db.couch.Couch
+import kpn.core.db.views.AnalyzerDesign
+import kpn.core.db.views.NodeNetworkReferenceView
+
+object NetworksWithNodesWithRoleConnection extends App {
+
+  Couch.executeIn("localhost", "master") { database =>
+    val nodeNetworkReferences = database.query(AnalyzerDesign, NodeNetworkReferenceView, Couch.batchTimeout, stale = false)().map(NodeNetworkReferenceView.convert)
+    val networks = nodeNetworkReferences.filter(_.nodeRoleConnection).map(n => (n.networkId, n.networkName)).distinct
+    networks.foreach { network =>
+      println(s"network id=${network._1}, name=${network._2}")
+    }
+  }
+}
