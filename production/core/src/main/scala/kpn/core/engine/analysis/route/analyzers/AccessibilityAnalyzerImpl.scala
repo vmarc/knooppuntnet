69e028518d8612c03ff542d8ffe09c8b7cc90d6d
vmarc <marc@vmarc.be>
2018-10-05 21:27:16 +0200

    take into account tags that forbid access for mode of transportation during accessibility analysis

--- production/core/src/main/scala/kpn/core/engine/analysis/route/analyzers/AccessibilityAnalyzerImpl.scala
+++ production/core/src/main/scala/kpn/core/engine/analysis/route/analyzers/AccessibilityAnalyzerImpl.scala
@@ -17,24 +17,27 @@ class AccessibilityAnalyzerImpl extends AccessibilityAnalyzer {
   }
 
   private def bicycleAccessible(way: Way): Boolean = {
-    way.tags.has("highway") ||
+    (way.tags.has("highway") ||
       way.tags.has("highway:virtual") ||
       way.tags.has("route", "ferry") ||
-      way.tags.has("bicycle", "yes")
+      way.tags.has("bicycle", "yes")) &&
+      !way.tags.has("bicycle", "no")
   }
 
   private def hikingAccessible(way: Way): Boolean = {
-    way.tags.has("highway") ||
+    (way.tags.has("highway") ||
       way.tags.has("highway:virtual") ||
       way.tags.has("route", "ferry") ||
-      way.tags.has("foot", "yes")
+      way.tags.has("foot", "yes")) &&
+      !way.tags.has("foot", "no")
   }
 
   private def horseAccessible(way: Way): Boolean = {
-    way.tags.has("highway") ||
+    (way.tags.has("highway") ||
       way.tags.has("highway:virtual") ||
       way.tags.has("route", "ferry") ||
-      way.tags.has("horse", "yes")
+      way.tags.has("horse", "yes")) &&
+      !way.tags.has("horse", "no")
   }
 
   private def motorboatAccessible(way: Way): Boolean = {
@@ -43,17 +46,19 @@ class AccessibilityAnalyzerImpl extends AccessibilityAnalyzer {
   }
 
   private def canoeAccessible(way: Way): Boolean = {
-    way.tags.has("waterway") ||
+    (way.tags.has("waterway") ||
       way.tags.has("waterway:virtual") ||
       way.tags.has("canoe", "portage") || // used for places where canoe has to be carried over a dam
-      way.tags.has("canoe", "yes")
+      way.tags.has("canoe", "yes")) &&
+      !way.tags.has("canoe", "no")
   }
 
   private def inlineSkatesAccessible(way: Way): Boolean = {
-    way.tags.has("highway") ||
+    (way.tags.has("highway") ||
       way.tags.has("highway:virtual") ||
       way.tags.has("route", "ferry") ||
-      way.tags.has("inline_skates", "yes")
+      way.tags.has("inline_skates", "yes")) &&
+      !way.tags.has("inline_skates", "no")
   }
 
 }
