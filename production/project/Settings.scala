f5cf2e51916a5bc4754736acb481ace29e65aa72
vmarc <marc@vmarc.be>
2018-10-18 00:51:22 +0200

    prepare version 2.0.12

--- production/project/Settings.scala
+++ production/project/Settings.scala
@@ -5,7 +5,7 @@ object Settings {
 
   val name = "kpn"
 
-  val version = "2.0.12-SNAPSHOT" // keep in sync: Version.current
+  val version = "2.0.12" // keep in sync: Version.current
 
   val scalacOptions = Seq(
     "-deprecation",
