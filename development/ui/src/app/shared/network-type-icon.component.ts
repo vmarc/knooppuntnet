0372985ded3259bb359a24c9eced9c7d3f0b4f25
vmarc <marc@vmarc.be>
2019-02-04 22:58:00 +0100

    dev - wip - network type icons

--- development/ui/src/app/shared/network-type-icon.component.ts
+++ development/ui/src/app/shared/network-type-icon.component.ts
@@ -4,8 +4,12 @@ import {NetworkType} from "../kpn/shared/network-type";
 @Component({
   selector: 'kpn-network-type-icon',
   template: `
-    <mat-icon *ngIf="networkType?.name == 'rcn'">directions_bike</mat-icon>
-    <mat-icon *ngIf="networkType?.name == 'rwn'">directions_walk</mat-icon>
+    <mat-icon svgIcon="rcn" *ngIf="networkType?.name == 'rcn'"></mat-icon>
+    <mat-icon svgIcon="rwn" *ngIf="networkType?.name == 'rwn'"></mat-icon>
+    <mat-icon svgIcon="rhn" *ngIf="networkType?.name == 'rhn'"></mat-icon>
+    <mat-icon svgIcon="rmn" *ngIf="networkType?.name == 'rmn'"></mat-icon>
+    <mat-icon svgIcon="rpn" *ngIf="networkType?.name == 'rpn'"></mat-icon>
+    <mat-icon svgIcon="rin" *ngIf="networkType?.name == 'rin'"></mat-icon>
   `
 })
 export class NetworkTypeIconComponent {
