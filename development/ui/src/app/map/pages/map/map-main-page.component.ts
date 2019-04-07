65f29f608530a48cbce9ca862cb7929eec30a9d8
vmarc <marc@vmarc.be>
2019-04-07 10:54:51 +0200

    dev - wip - integrate planner functionality in map page

--- development/ui/src/app/map/pages/map/map-main-page.component.ts
+++ development/ui/src/app/map/pages/map/map-main-page.component.ts
@@ -1,15 +1,35 @@
 import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import Coordinate from 'ol/coordinate';
+import {click, pointerMove} from 'ol/events/condition';
+import Select from 'ol/interaction/Select';
+import TileLayer from 'ol/layer/Tile';
+import VectorTileLayer from 'ol/layer/VectorTile';
+import Map from 'ol/Map';
+import fromLonLat from 'ol/proj';
+import Style from 'ol/style';
+import View from 'ol/View';
+import Extent from 'ol/View';
 import {Subscription} from "rxjs";
+import {DebugLayer} from "../../../components/ol/domain/debug-layer";
+import {MainMapStyle} from "../../../components/ol/domain/main-map-style";
+import {MapClickHandler} from "../../../components/ol/domain/map-click-handler";
+import {MapMoveHandler} from "../../../components/ol/domain/map-move-handler";
+import {NetworkBitmapTileLayer} from "../../../components/ol/domain/network-bitmap-tile-layer";
+import {NetworkVectorTileLayer} from "../../../components/ol/domain/network-vector-tile-layer";
+import {OsmLayer} from "../../../components/ol/domain/osm-layer";
+import {ZoomLevel} from "../../../components/ol/domain/zoom-level";
 import {MapComponent} from "../../../components/ol/map.component";
 import {MapService} from "../../../components/ol/map.service";
+import {PoiTileLayerService} from "../../../components/ol/poi-tile-layer.service";
 import {PageService} from "../../../components/shared/page.service";
 import {NetworkType} from "../../../kpn/shared/network-type";
+import {PoiService} from "../../../poi.service";
 
 @Component({
   selector: 'kpn-map-main-page',
   template: `
-    <kpn-map id="main-map" class="map"></kpn-map>
+    <div id="main-map" class="map"></div>
   `,
   styles: [`
     .map {
@@ -23,16 +43,25 @@ import {NetworkType} from "../../../kpn/shared/network-type";
 })
 export class MapMainPageComponent implements OnInit, OnDestroy {
 
+  map: Map;
+  mainMapStyle: (feature, resolution) => Style;
+
+  bitmapTileLayer: TileLayer;
+  vectorTileLayer: VectorTileLayer;
+  poiTileLayer: VectorTileLayer;
+
   paramsSubscription: Subscription;
   selectedFeatureSubscription: Subscription;
 
   private lastKnownSidebarOpen = false;
 
-  @ViewChild(MapComponent) map: MapComponent;
+  @ViewChild(MapComponent) mapComponent: MapComponent;
 
   constructor(private activatedRoute: ActivatedRoute,
               private pageService: PageService,
-              private mapService: MapService) {
+              private mapService: MapService,
+              private poiService: PoiService,
+              private poiTileLayerService: PoiTileLayerService) {
   }
 
   ngOnInit() {
@@ -55,12 +84,111 @@ export class MapMainPageComponent implements OnInit, OnDestroy {
   ngAfterContentChecked() {
     if (this.lastKnownSidebarOpen !== this.pageService.sidebarOpen.value) {
       this.lastKnownSidebarOpen = this.pageService.sidebarOpen.value;
-      setTimeout(() => this.map.updateSize(), 500);
+      setTimeout(() => this.mapComponent.updateSize(), 500);
     }
+  }
+
+  ngAfterViewInit(): void {
+
+    this.bitmapTileLayer = NetworkBitmapTileLayer.build(this.mapService.networkType.value);
+    this.vectorTileLayer = NetworkVectorTileLayer.build(this.mapService.networkType.value);
+    this.poiTileLayer = this.poiTileLayerService.buildLayer();
+
+    this.map = new Map({
+      target: "main-map",
+      layers: [
+        OsmLayer.build(),
+        this.poiTileLayer,
+        this.bitmapTileLayer,
+        this.vectorTileLayer,
+        DebugLayer.build()
+      ],
+      view: new View({
+        minZoom: ZoomLevel.minZoom,
+        maxZoom: ZoomLevel.maxZoom
+      })
+    });
+
+    this.mainMapStyle = new MainMapStyle(this.map, this.mapService).styleFunction();
+
+    this.installClickInteraction();
+    this.installMoveInteraction();
+
+    const a: Coordinate = fromLonLat([2.24, 50.16]);
+    const b: Coordinate = fromLonLat([10.56, 54.09]);
+    const extent: Extent = [a[0], a[1], b[0], b[1]];
+    const view = this.map.getView();
+    view.fit(extent);
+    view.on("change:resolution", () => this.zoom(view.getZoom()));
+
+    this.mapService.networkType.subscribe(networkType => {
+      this.bitmapTileLayer = NetworkBitmapTileLayer.build(networkType);
+      this.vectorTileLayer = NetworkVectorTileLayer.build(networkType);
+      this.vectorTileLayer.setStyle(this.mainMapStyle);
+      this.map.getLayers().removeAt(2);
+      this.map.getLayers().insertAt(2, this.bitmapTileLayer);
+      this.map.getLayers().removeAt(3);
+      this.map.getLayers().insertAt(3, this.vectorTileLayer);
+      this.updateLayerVisibility(view.getZoom());
+    });
+
   }
 
   ngOnDestroy() {
     this.paramsSubscription.unsubscribe();
     this.selectedFeatureSubscription.unsubscribe();
   }
+
+  private zoom(zoomLevel: number) {
+    this.updateLayerVisibility(zoomLevel);
+    this.poiService.updateZoomLevel(zoomLevel);
+    return true;
+  }
+
+  private updateLayerVisibility(zoomLevel: number) {
+    const zoom = Math.round(zoomLevel);
+    if (zoom <= ZoomLevel.bitmapTileMaxZoom) {
+      this.bitmapTileLayer.setVisible(true);
+      this.vectorTileLayer.setVisible(false);
+    } else if (zoom >= ZoomLevel.vectorTileMinZoom) {
+      this.bitmapTileLayer.setVisible(false);
+      this.vectorTileLayer.setVisible(true);
+    }
+    if (zoom >= 11) {
+      this.poiTileLayer.setVisible(true);
+    } else if (zoom >= ZoomLevel.vectorTileMinZoom) {
+      this.poiTileLayer.setVisible(false);
+    }
+  }
+
+
+  private installClickInteraction() {
+    const interaction = new Select({
+      condition: click,
+      multi: false,
+      style: new Style() // this overrides the normal openlayers default edit style
+    });
+    interaction.on("select", (e) => {
+      new MapClickHandler(this.mapService).handle(e);
+      this.vectorTileLayer.changed();
+      return true;
+    });
+    this.map.addInteraction(interaction);
+
+  }
+
+  private installMoveInteraction() {
+    const interaction = new Select({
+      condition: pointerMove,
+      multi: false,
+      style: new Style() // this overrides the normal openlayers default edit style
+    });
+    interaction.on("select", (e) => {
+      new MapMoveHandler(this.map, this.mapService).handle(e);
+      this.vectorTileLayer.changed();
+      return true;
+    });
+    this.map.addInteraction(interaction);
+  }
+
 }
