64fd65b787b44b3d17d2653f220936089c4bb7b2
vmarc <marc@vmarc.be>
2019-11-03 13:19:14 +0100

    fix orphan node view (network types rhn, rmn, rpn and rin were being ignored)

--- production/core/src/main/resources/kpn/core/db/views/OrphanNodeView-map.js
+++ production/core/src/main/resources/kpn/core/db/views/OrphanNodeView-map.js
@@ -1,27 +1,72 @@
 if (doc && doc.node && doc.node.active === true) {
-  var n = doc.node;
-  if (n.active === true && (n.orphan === true || n.ignored === true)) {
-    if (n.rwnName.length > 0) {
-      var key = [
-        n.ignored,
-        n.orphan,
-        n.display,
-        n.country,
-        "rwn",
-        n.id
-      ];
-      emit(key, n);
+    var n = doc.node;
+    if (n.active === true && (n.orphan === true || n.ignored === true)) {
+        if (n.rwnName.length > 0) {
+            var key = [
+                n.ignored,
+                n.orphan,
+                n.display,
+                n.country,
+                "rwn",
+                n.id
+            ];
+            emit(key, n);
+        }
+        if (n.rcnName.length > 0) {
+            var key = [
+                n.ignored,
+                n.orphan,
+                n.display,
+                n.country,
+                "rcn",
+                n.id
+            ];
+            emit(key, n);
+        }
+
+        if (n.rhnName.length > 0) {
+            var key = [
+                n.ignored,
+                n.orphan,
+                n.display,
+                n.country,
+                "rhn",
+                n.id
+            ];
+            emit(key, n);
+        }
+        if (n.rmnName.length > 0) {
+            var key = [
+                n.ignored,
+                n.orphan,
+                n.display,
+                n.country,
+                "rmn",
+                n.id
+            ];
+            emit(key, n);
+        }
+        if (n.rpnName.length > 0) {
+            var key = [
+                n.ignored,
+                n.orphan,
+                n.display,
+                n.country,
+                "rpn",
+                n.id
+            ];
+            emit(key, n);
+        }
+        if (n.rinName.length > 0) {
+            var key = [
+                n.ignored,
+                n.orphan,
+                n.display,
+                n.country,
+                "rin",
+                n.id
+            ];
+            emit(key, n);
+        }
     }
-    if (n.rcnName.length > 0) {
-      var key = [
-        n.ignored,
-        n.orphan,
-        n.display,
-        n.country,
-        "rcn",
-        n.id
-      ];
-      emit(key, n);
-    }
-  }
 }
