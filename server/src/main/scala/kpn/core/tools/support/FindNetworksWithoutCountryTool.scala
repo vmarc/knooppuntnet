85f78d6e22c8473cfffdf8bc4cbe64b485b3ed30
vmarc <marc@vmarc.be>
2019-11-16 17:40:27 +0100

    migrate to Spring boot - wip - investigate networks without country

---
+++ server/src/main/scala/kpn/core/tools/support/FindNetworksWithoutCountryTool.scala
@@ -0,0 +1,25 @@
+package kpn.core.tools.support
+
+import kpn.api.custom.Country
+import kpn.api.custom.NetworkType
+import kpn.core.db.couch.Couch
+import kpn.server.repository.NetworkRepositoryImpl
+
+object FindNetworksWithoutCountryTool {
+  def main(args: Array[String]): Unit = {
+    Couch.executeIn("kpn-server", "master-test") { database =>
+      val repo = new NetworkRepositoryImpl(database)
+      val networks = NetworkType.all.flatMap { networkType =>
+        repo.networks(kpn.api.custom.Subset(Country.nl, networkType), Couch.defaultTimeout, stale = true).filter(_.country.isEmpty)
+      }
+      println(networks.map(_.id).mkString(","))
+      println(networks.map(_.name).mkString(","))
+      println("Done")
+    }
+  }
+
+}
+
+class FindNetworksWithoutCountryTool {
+
+}
