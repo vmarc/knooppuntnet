8777766c166fdfd6310307f41391897fbb795313
vmarc <marc@vmarc.be>
2019-12-30 21:20:16 +0100

    improve point of interest processing: interprete "heritage:website" tag for "Onroerend Erfgoed"

--- server/src/main/scala/kpn/server/api/analysis/pages/poi/analyzers/OnroerendErfgoedAnalyzer.scala
+++ server/src/main/scala/kpn/server/api/analysis/pages/poi/analyzers/OnroerendErfgoedAnalyzer.scala
@@ -14,16 +14,32 @@ class OnroerendErfgoedAnalyzer(context: PoiAnalysisContext) {
   def analyze: PoiAnalysisContext = {
     context.poi.tags("ref:OnroerendErfgoed") match {
       case Some(ref) => processRef(ref)
-      case _ => context
+      case _ =>
+        context.poi.tags("heritage:website") match {
+          case Some(website) if website.contains("inventaris.onroerenderfgoed.be") => processWebsite(website)
+          case _ => context
+        }
     }
   }
 
   private def processRef(ref: String): PoiAnalysisContext = {
-
     val onroerendErfgoed = s"https://inventaris.onroerenderfgoed.be/dibe/relict/$ref"
     context.copy(
       analysis = context.analysis.copy(onroerendErfgoed = Some(onroerendErfgoed)),
       processedTagKeys = context.processedTagKeys ++ Seq("ref:OnroerendErfgoed"),
+      ignoredTagKeys = context.ignoredTagKeys ++ Seq(
+        "OnroerendErfgoed:criteria",
+        "heritage",
+        "heritage:operator",
+        "heritage:website"
+      )
+    )
+  }
+
+  private def processWebsite(website: String): PoiAnalysisContext = {
+    context.copy(
+      analysis = context.analysis.copy(onroerendErfgoed = Some(website)),
+      processedTagKeys = context.processedTagKeys ++ Seq("heritage:website"),
       ignoredTagKeys = context.ignoredTagKeys ++ Seq(
         "OnroerendErfgoed:criteria",
         "heritage",
