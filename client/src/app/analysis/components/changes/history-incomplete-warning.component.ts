96723fa253a824553bd2d60a5bbad0bab45e6369
vmarc <marc@vmarc.be>
2020-02-18 23:00:08 +0100

    cleanup and translations

--- client/src/app/analysis/components/changes/history-incomplete-warning.component.ts
+++ client/src/app/analysis/components/changes/history-incomplete-warning.component.ts
@@ -3,13 +3,10 @@ import {Component} from "@angular/core";
 @Component({
   selector: "kpn-history-incomplete-warning",
   template: `
-    <p class="note">
-      <span i18n="@@history-incomplete.warning">
-        Older changes cannot be shown. The history in the analysis database 
-        does not go beyond
-      </span>
-      2012-09-12 (
-      <a class="external" href="https://wiki.openstreetmap.org/wiki/NL:Open_Database_License" target="_blank" i18n="@@history-incomplete.license-change">
+    <p class="note" i18n="@@history-incomplete.warning">
+      Older changes cannot be shown. The history in the analysis database
+      does not go beyond 2012-09-12 (
+      <a class="external" href="https://wiki.openstreetmap.org/wiki/NL:Open_Database_License" target="_blank">
         License change
       </a>).
     </p>
