972bfa5eed348ef6b079fb41220e9f2260bf4523
vmarc <marc@vmarc.be>
2020-03-15 10:47:08 +0100

    #71 reproduce problem with overlapping ways

---
+++ server/src/test/scala/kpn/server/analyzer/engine/analysis/caseStudies/Issue71.scala
@@ -0,0 +1,19 @@
+package kpn.server.analyzer.engine.analysis.caseStudies
+
+import kpn.api.custom.Fact
+import org.scalatest.FunSuite
+import org.scalatest.Matchers
+
+class Issue71 extends FunSuite with Matchers {
+
+  test("route 17-19") {
+    val route = CaseStudy.routeAnalysis("9637368").route
+    route.facts should equal(Seq(Fact.RouteUnusedSegments, Fact.RouteInvalidSortingOrder, Fact.RouteBroken))
+  }
+
+  test("route 314-353") {
+    val route = CaseStudy.routeAnalysis("1245740").route
+    route.facts should equal(Seq(Fact.RouteInvalidSortingOrder))
+  }
+
+}
