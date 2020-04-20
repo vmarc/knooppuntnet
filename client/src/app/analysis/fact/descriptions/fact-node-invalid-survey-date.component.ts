5c170486372a231309cca750250e1b19909baec7
vmarc <marc@vmarc.be>
2020-04-20 22:33:15 +0200

    survey date analysis - wip

--- client/src/app/analysis/fact/descriptions/fact-node-invalid-survey-date.component.ts
+++ client/src/app/analysis/fact/descriptions/fact-node-invalid-survey-date.component.ts
@@ -1,21 +1,13 @@
-import { Component, OnInit } from "@angular/core";
+import {Component} from "@angular/core";
 
 @Component({
   selector: "kpn-fact-node-invalid-survey-date",
   template: `
     <markdown i18n="@@fact.description.node-invalid-survey-date">
-      The node survey date (in the _"survey:date"_ or _"source:date"_ tag) has an
-      unexpected format (valid format: YYYY-MM-DD or YYYY-MM).
+      The node survey date has an unexpected format (valid format: YYYY-MM-DD or YYYY-MM).
+      The survey date is defined in the _"survey:date"_ tag, or the _"source:date"_ tag if _"source=survey"_.
     </markdown>
-  `,
-  styles: [
-  ],
+  `
 })
-export class FactNodeInvalidSurveyDateComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class FactNodeInvalidSurveyDateComponent {
 }
---
+++ client/src/app/analysis/fact/descriptions/fact-route-invalid-survey-date.component.ts
@@ -0,0 +1,13 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: "kpn-fact-route-invalid-survey-date",
+  template: `
+    <markdown i18n="@@fact.description.route-invalid-survey-date">
+      The route survey date has an unexpected format (valid format: YYYY-MM-DD or YYYY-MM).
+      The survey date is defined in the _"survey:date"_ tag, or the _"source:date"_ tag if _"source=survey"_.
+    </markdown>
+  `
+})
+export class FactRouteInvalidSurveyDateComponent {
+}
