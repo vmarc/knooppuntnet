bfa258acec62bd5c334b7a74d7db38fda9417a05
vmarc <marc@vmarc.be>
2020-08-13 21:09:55 +0200

    do not autoupdate couchdb views

--- server/src/main/scala/kpn/core/database/doc/DesignDoc.scala
+++ server/src/main/scala/kpn/core/database/doc/DesignDoc.scala
@@ -4,6 +4,7 @@ case class DesignDoc(
   _id: String,
   language: String,
   views: Map[String, ViewDoc] = Map.empty,
+  autoupdate: Boolean = false,
   _rev: Option[String] = None
 ) extends Doc {
   def withRev(_newRev: Option[String]): Doc = this.copy(_rev = _newRev)
