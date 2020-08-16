d57b3ac3341108599993241f6729027b7b6f50c4
vmarc <marc@vmarc.be>
2020-08-16 20:11:56 +0200

    re-enable couchdb indexing

--- server/src/main/scala/kpn/core/database/doc/DesignDoc.scala
+++ server/src/main/scala/kpn/core/database/doc/DesignDoc.scala
@@ -4,7 +4,6 @@ case class DesignDoc(
   _id: String,
   language: String,
   views: Map[String, ViewDoc] = Map.empty,
-  autoupdate: Boolean = false,
   _rev: Option[String] = None
 ) extends Doc {
   def withRev(_newRev: Option[String]): Doc = this.copy(_rev = _newRev)
