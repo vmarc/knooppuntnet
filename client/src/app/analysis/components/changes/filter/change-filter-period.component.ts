06c4c78008417fce2f598d5e7a81459c470829d0
vmarc <marc@vmarc.be>
2020-08-31 21:50:10 +0200

    use month names instead of numbers in changes pages filters

--- client/src/app/analysis/components/changes/filter/change-filter-period.component.ts
+++ client/src/app/analysis/components/changes/filter/change-filter-period.component.ts
@@ -9,7 +9,23 @@ import {ChangeFilterOption} from "./change-filter-option";
   template: `
     <div class="row">
       <div [ngClass]="option.level">
-        {{option.period.name}}
+        <ng-container *ngIf="option.level === 'month'">
+          <span *ngIf="option.period.name === '01'" i18n="@@filter.month.january">January</span>
+          <span *ngIf="option.period.name === '02'" i18n="@@filter.month.february">February</span>
+          <span *ngIf="option.period.name === '03'" i18n="@@filter.month.march">March</span>
+          <span *ngIf="option.period.name === '04'" i18n="@@filter.month.april">April</span>
+          <span *ngIf="option.period.name === '05'" i18n="@@filter.month.may">May</span>
+          <span *ngIf="option.period.name === '06'" i18n="@@filter.month.june">June</span>
+          <span *ngIf="option.period.name === '07'" i18n="@@filter.month.july">July</span>
+          <span *ngIf="option.period.name === '08'" i18n="@@filter.month.august">August</span>
+          <span *ngIf="option.period.name === '09'" i18n="@@filter.month.september">September</span>
+          <span *ngIf="option.period.name === '10'" i18n="@@filter.month.october">October</span>
+          <span *ngIf="option.period.name === '11'" i18n="@@filter.month.november">November</span>
+          <span *ngIf="option.period.name === '12'" i18n="@@filter.month.december">December</span>
+        </ng-container>
+        <ng-container *ngIf="option.level !== 'month'">
+          {{option.period.name}}
+        </ng-container>
         <ng-container *ngIf="option.period.current">*</ng-container>
       </div>
       <div class="count-links">
