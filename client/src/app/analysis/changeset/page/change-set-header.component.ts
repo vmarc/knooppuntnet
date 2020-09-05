208bc278c6d63f2ad1975e6a7929f53c4cface80
vmarc <marc@vmarc.be>
2020-09-05 11:14:18 +0200

    add link to osmcha in changeset page

--- client/src/app/analysis/changeset/page/change-set-header.component.ts
+++ client/src/app/analysis/changeset/page/change-set-header.component.ts
@@ -17,13 +17,26 @@ import {ChangeSetPage} from "../../../kpn/api/common/changes/change-set-page";
           <div class="kpn-line">
             <span>{{page.summary.key.changeSetId}}</span>
             <kpn-osm-link-change-set [changeSetId]="page.summary.key.changeSetId"></kpn-osm-link-change-set>
+            <span>
             <a
               class="external"
               rel="nofollow noreferrer"
               target="_blank"
-              [href]="'https://overpass-api.de/achavi/?changeset=' + page.summary.key.changeSetId" i18n="@@change-set.header.achavi">
+              [href]="'https://overpass-api.de/achavi/?changeset=' + page.summary.key.changeSetId"
+              i18n="@@change-set.header.achavi">
               achavi
             </a>
+            </span>
+            <span>
+            <a
+              class="external"
+              rel="nofollow noreferrer"
+              target="_blank"
+              [href]="'https://osmcha.org/changesets/' + page.summary.key.changeSetId"
+              i18n="@@change-set.header.osmcha">
+              osmcha
+            </a>
+            </span>
           </div>
         </td>
       </tr>
