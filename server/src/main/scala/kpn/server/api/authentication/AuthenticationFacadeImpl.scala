eece1df1be9df5ca4d977b57a61c68255fb90599
vmarc <marc@vmarc.be>
2020-10-18 21:34:53 +0200

    #113 explicitely request xml response when retrieving user details from openstreetmap api

--- server/src/main/scala/kpn/server/api/authentication/AuthenticationFacadeImpl.scala
+++ server/src/main/scala/kpn/server/api/authentication/AuthenticationFacadeImpl.scala
@@ -1,6 +1,16 @@
 package kpn.server.api.authentication
 
+import java.nio.charset.Charset
+
 import kpn.core.util.Log
+import kpn.server.analyzer.engine.changes.changes.ChangeSetInfoParser
+import org.apache.commons.io.FileUtils
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.social.oauth1.AuthorizedRequestToken
 import org.springframework.social.oauth1.GenericOAuth1ServiceProvider
 import org.springframework.social.oauth1.OAuth1Template
@@ -51,8 +61,25 @@ class AuthenticationFacadeImpl(
     )
 
     val op: RestOperations = provider.getApi(accessToken.getValue, accessToken.getSecret)
-    val userXml = op.getForObject("https://api.openstreetmap.org/api/0.6/user/details", classOf[String])
-    new UserParser().parse(XML.loadString(userXml)).get
-  }
 
+    val url = "https://api.openstreetmap.org/api/0.6/user/details"
+    val headers = new HttpHeaders()
+    headers.setAccept(java.util.Arrays.asList(MediaType.TEXT_XML))
+    headers.setAcceptCharset(java.util.Arrays.asList(Charset.forName("UTF-8")))
+    headers.set(HttpHeaders.REFERER, "knooppuntnet.nl")
+    val entity = new HttpEntity[String]("", headers)
+
+    try {
+      val response: ResponseEntity[String] = op.exchange(url, HttpMethod.GET, entity, classOf[String])
+      response.getStatusCode match {
+        case HttpStatus.OK =>
+          val userXml = response.getBody
+          log.info(userXml)
+          new UserParser().parse(XML.loadString(userXml)).get
+        case _ =>
+          log.error(s"Could not retrieve user xml from OSM API")
+          ""
+      }
+    }
+  }
 }
