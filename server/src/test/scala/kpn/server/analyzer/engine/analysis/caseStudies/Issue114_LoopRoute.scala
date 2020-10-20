d66ebcedd6d166e80e090b41d35a068599915e40
vmarc <marc@vmarc.be>
2020-10-20 23:35:49 +0200

    #114 loop route - added tests illustrating the problem

---
+++ server/src/test/scala/kpn/server/analyzer/engine/analysis/caseStudies/Issue114_LoopRoute.scala
@@ -0,0 +1,17 @@
+package kpn.server.analyzer.engine.analysis.caseStudies
+
+import kpn.core.util.UnitTest
+
+class Issue114_LoopRoute extends UnitTest {
+
+  test("route 62-62") {
+    val route = CaseStudy.routeAnalysis("11772920").route
+    route.facts should equal(Seq())
+  }
+
+  test("route 4-4") {
+    val route = CaseStudy.routeAnalysis("11659448").route
+    route.facts should equal(Seq())
+  }
+
+}
