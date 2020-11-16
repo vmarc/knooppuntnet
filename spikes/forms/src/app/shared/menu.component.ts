4b66b25e5f3b26e505a563cfa7f855116600126e
vmarc <marc@vmarc.be>
2020-11-16 19:38:23 +0100

    forms spike - update @angular/core and @angular/cli version 11

--- spikes/forms/src/app/shared/menu.component.ts
+++ spikes/forms/src/app/shared/menu.component.ts
@@ -3,9 +3,9 @@ import {Component} from "@angular/core";
 @Component({
   selector: "app-menu",
   template: `
-    <a routerLink="/page1">page 1</a> |
-    <a routerLink="/page2">page 2</a> |
-    <a routerLink="/page3">page 3</a>
+    <a routerLink="../page1">page 1</a> |
+    <a routerLink="../page2">page 2</a> |
+    <a routerLink="../page3">page 3</a>
   `
 })
 export class MenuComponent {
