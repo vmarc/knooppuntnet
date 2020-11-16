160ece9ac00fd89373f14e9d5412931398d2ce25
vmarc <marc@vmarc.be>
2020-11-16 21:59:02 +0100

    update @angular/material version 11

--- spikes/ngrx/src/app/home/home.component.ts
+++ spikes/ngrx/src/app/home/home.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-home',
   template: `
-    <a routerLink="counter">counter</a>
+    <a routerLink="../counter">counter</a>
   `
 })
 export class HomeComponent {
