a653d918a63cfb31777f98a0b5e08043e44df1e2
Marc Vergouwen <marc.vergouwen@dxcfds.com>
2021-06-30 20:02:38 +0200

    adapt replication state reader to new state file contents + Tags serialization

--- server/src/main/scala/kpn/server/json/TagsJsonDeserializer.scala
+++ server/src/main/scala/kpn/server/json/TagsJsonDeserializer.scala
@@ -6,33 +6,47 @@ import com.fasterxml.jackson.databind.DeserializationContext
 import com.fasterxml.jackson.databind.JsonDeserializer
 import com.fasterxml.jackson.databind.JsonMappingException
 import com.fasterxml.jackson.databind.node.ArrayNode
+import kpn.api.custom.Tag
 import kpn.api.custom.Tags
 import org.apache.commons.text.StringEscapeUtils
 
 class TagsJsonDeserializer extends JsonDeserializer[Tags] {
+
   override def deserialize(jsonParser: JsonParser, deserializationContext: DeserializationContext): Tags = {
     val treeNode: TreeNode = jsonParser.getCodec.readTree(jsonParser)
-    val pairs: Seq[(String, String)] = treeNode match {
-      case arrayNode: ArrayNode =>
-        import scala.jdk.CollectionConverters._
-        arrayNode.elements().asScala.map {
-          case pairArrayNode: ArrayNode if pairArrayNode.size() == 2 =>
-            val key = StringEscapeUtils.unescapeJson(pairArrayNode.get(0).textValue)
-            val value = StringEscapeUtils.unescapeJson(pairArrayNode.get(1).textValue)
-            key -> value
-          case _ =>
-            throw JsonMappingException.from(
-              jsonParser,
-              "Cannot deserialize tags"
-            )
-        }.toSeq
-
-      case _ =>
-        throw JsonMappingException.from(
-          jsonParser,
-          "Cannot deserialize tags"
-        )
+    if (treeNode.isObject) {
+      // normal tree structure
+      val tagsNode = treeNode.get("tags")
+      val tags = (0 until tagsNode.size()).map { index =>
+        val tagNode = tagsNode.get(index)
+        jsonParser.getCodec.treeToValue(tagNode, classOf[Tag])
+      }
+      Tags(tags)
     }
-    Tags.from(pairs: _*)
+    else {
+      // for backward compatibility
+      val pairs: Seq[(String, String)] = treeNode match {
+        case arrayNode: ArrayNode =>
+          import scala.jdk.CollectionConverters._
+          arrayNode.elements().asScala.map {
+            case pairArrayNode: ArrayNode if pairArrayNode.size() == 2 =>
+              val key = StringEscapeUtils.unescapeJson(pairArrayNode.get(0).textValue)
+              val value = StringEscapeUtils.unescapeJson(pairArrayNode.get(1).textValue)
+              key -> value
+            case _ =>
+              throw JsonMappingException.from(
+                jsonParser,
+                "Cannot deserialize tags"
+              )
+          }.toSeq
+
+        case _ =>
+          throw JsonMappingException.from(
+            jsonParser,
+            "Cannot deserialize tags"
+          )
+      }
+      Tags.from(pairs: _*)
+    }
   }
 }
