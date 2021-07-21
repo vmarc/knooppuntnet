05ed359e7afc456b30f787dd5dd7cc7b7192bf73
vmarc <marc@vmarc.be>
2021-07-21 13:28:32 +0200

    #114 verify route 4-4 analysis

--- server/src/test/scala/kpn/server/analyzer/engine/analysis/caseStudies/Issue114_LoopRoute.scala
+++ server/src/test/scala/kpn/server/analyzer/engine/analysis/caseStudies/Issue114_LoopRoute.scala
@@ -1,5 +1,6 @@
 package kpn.server.analyzer.engine.analysis.caseStudies
 
+import kpn.api.common.route.RouteNetworkNodeInfo
 import kpn.core.util.UnitTest
 
 class Issue114_LoopRoute extends UnitTest {
@@ -12,7 +13,25 @@ class Issue114_LoopRoute extends UnitTest {
 
   test("route 4-4") {
     val route = CaseStudy.routeAnalysis("11659448").route
+
     route.facts shouldBe empty
+
+    route.analysis.map.freeNodes should matchTo {
+      Seq(
+        RouteNetworkNodeInfo(
+          47452329L,
+          "04",
+          "04",
+          "52.8022502",
+          "4.7589098"
+        )
+      )
+    }
+    route.analysis.map.freePaths.size should equal(1)
+    val path = route.analysis.map.freePaths.head
+    path.startNodeId should equal(47452329L)
+    path.endNodeId should equal(47452329L)
+    assert(path.meters > 7000)
   }
 
   test("route 30-30, not a loop but all nodes have the same name") {
