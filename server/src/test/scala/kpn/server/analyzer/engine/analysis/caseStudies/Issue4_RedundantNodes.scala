598a7db5313d768927a3d930b4507f36bddf094e
vmarc <marc@vmarc.be>
2021-07-22 18:43:57 +0200

    #4 reproduce problem in unit test

---
+++ server/src/test/scala/kpn/server/analyzer/engine/analysis/caseStudies/Issue4_RedundantNodes.scala
@@ -0,0 +1,71 @@
+package kpn.server.analyzer.engine.analysis.caseStudies
+
+import kpn.api.common.LatLonImpl
+import kpn.api.common.common.TrackSegment
+import kpn.api.custom.Fact
+import kpn.core.tools.`export`.GeoJsonLineStringGeometry
+import kpn.core.util.UnitTest
+import kpn.server.json.Json
+
+class Issue4_RedundantNodes extends UnitTest {
+
+  test("route 21-70") {
+    val route = CaseStudy.routeAnalysis("3792006").route
+    route.facts should equal(Seq.empty)
+  }
+
+  test("route 24-32") {
+    val route = CaseStudy.routeAnalysis("3330377").route
+
+    println(route.analysis.structureStrings)
+    route.analysis.map.unusedSegments.zipWithIndex.foreach { case (segment, index) =>
+      makeGeojson(s"unusedSegment ${index + 1}", segment)
+    }
+
+    route.analysis.map.startNodes.map(_.name).sorted should equal(Seq("24"))
+    route.analysis.map.endNodes.map(_.name).sorted should equal(Seq("32"))
+    route.analysis.map.redundantNodes.map(_.name).sorted should equal(
+      Seq(
+        "83",
+        "84",
+        "85",
+        "86"
+      )
+    )
+
+    route.facts should equal(Seq(Fact.RouteRedundantNodes))
+  }
+
+  test("route 56-58") {
+    val route = CaseStudy.routeAnalysis("3715798").route
+    println(route.analysis.structureStrings)
+    route.analysis.map.unusedSegments.zipWithIndex.foreach { case (segment, index) =>
+      makeGeojson(s"unusedSegment ${index + 1}", segment)
+    }
+    route.analysis.map.startNodes.map(_.name).sorted should equal(Seq("56"))
+    route.analysis.map.endNodes.map(_.name).sorted should equal(Seq("58"))
+    route.analysis.map.redundantNodes.map(_.name).sorted should equal(
+      Seq(
+        "03",
+        "04",
+        "07",
+        "08",
+        "24"
+      )
+    )
+
+    route.facts should equal(Seq(Fact.RouteRedundantNodes))
+  }
+
+  private def makeGeojson(name: String, trackSegment: TrackSegment): Unit = {
+    val latlons = trackSegment.trackPoints.map(point => LatLonImpl(point.lat, point.lon))
+    val coordinates = latlons.toArray.map(c => Array(c.lon, c.lat))
+    val line = GeoJsonLineStringGeometry(
+      "LineString",
+      coordinates
+    )
+    val json = Json.objectMapper.writerWithDefaultPrettyPrinter()
+    println(s"https://geojson.io/ $name")
+    println(json.writeValueAsString(line))
+  }
+}
