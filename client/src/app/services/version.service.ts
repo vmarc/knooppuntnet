57de95686f6923ae9a4e1e648da1039c2489e67d
vmarc <marc@vmarc.be>
2021-07-24 15:57:04 +0200

    user interface release 3.2.0-snapshot.3

--- client/src/app/services/version.service.ts
+++ client/src/app/services/version.service.ts
@@ -2,6 +2,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class VersionService {
-  version = '3.2.0-snapshot.2';
+  version = '3.2.0-snapshot.3';
   experimental = true; // see also: network-vector-tile-layer.ts tile url !!
 }
