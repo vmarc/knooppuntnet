6adb12aad7d4bf4ba0357159f99b48a90fc04711
vmarc <marc@vmarc.be>
2021-08-05 10:15:15 +0200

    improve overpass controller logging

--- server/src/main/scala/kpn/server/api/overpass/OverpassController.scala
+++ server/src/main/scala/kpn/server/api/overpass/OverpassController.scala
@@ -1,17 +1,40 @@
 package kpn.server.api.overpass
 
 import kpn.core.overpass.OverpassQueryExecutor
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import kpn.core.util.Log
+import org.springframework.web.bind.annotation.{PostMapping, RequestBody, RestController}
 
 @RestController
 class OverpassController(nonCachingOverpassQueryExecutor: OverpassQueryExecutor) {
 
+  private val log = Log(classOf[OverpassController])
+
   @PostMapping(value = Array("/api/overpass"))
-  def mapNodeDetail(
-    @RequestBody queryString: String
-  ): String = {
-    nonCachingOverpassQueryExecutor.execute(queryString)
+  def mapNodeDetail(@RequestBody queryString: String): String = {
+    log.elapsed {
+      val xml = try {
+        nonCachingOverpassQueryExecutor.execute(queryString)
+      }
+      catch {
+        case e: Exception =>
+          log.error(e.getMessage)
+          throw e
+      }
+      val message = shorten(queryString)
+      (message, xml)
+    }
   }
+
+  private def shorten(queryString: String): String = {
+    val string = queryString
+      .replaceAll("\\[timeout:1500]\\[maxsize:24000000000];", "")
+      .replaceAll("\\[timeout:500]\\[maxsize:12000000000];", "")
+    if (string.length > 300) {
+      string.take(300) + "..."
+    }
+    else {
+      string
+    }
+  }
+
 }
