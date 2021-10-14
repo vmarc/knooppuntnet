fa0c641b1a324bb75e823f2f434139126851ac76
vmarc <marc@vmarc.be>
2021-10-14 21:43:34 +0200

    #203 reproduce route analysis problem

---
+++ server/src/test/scala/kpn/server/analyzer/engine/analysis/caseStudies/Issue203_NamedRoutes.scala
@@ -0,0 +1,27 @@
+package kpn.server.analyzer.engine.analysis.caseStudies
+
+import kpn.api.custom.Fact
+import kpn.core.util.UnitTest
+
+class Issue203_NamedRoutes extends UnitTest {
+
+  test("Carrefour des Planches - Croix des Clos") {
+    val route = CaseStudy.routeAnalysis("13305500").route
+    route.facts should equal(
+      Fact.RouteRedundantNodes,
+      Fact.RouteNotForward,
+      Fact.RouteNotBackward,
+      Fact.RouteNotContinious,
+      Fact.RouteBroken
+    )
+  }
+
+  test("Le Villard - Le Villard") {
+    val route = CaseStudy.routeAnalysis("12219285").route
+    route.facts should equal(
+      Fact.RouteRedundantNodes,
+      Fact.RouteUnusedSegments,
+      Fact.RouteBroken
+    )
+  }
+}
