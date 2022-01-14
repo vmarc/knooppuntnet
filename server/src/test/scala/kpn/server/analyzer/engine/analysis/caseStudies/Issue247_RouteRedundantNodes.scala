3584b6e14041735e89e4bb6380eb79996ca5c361
vmarc <marc@vmarc.be>
2022-01-14 18:22:03 +0100

    #247 no redundant nodes anymore in release 4

---
+++ server/src/test/scala/kpn/server/analyzer/engine/analysis/caseStudies/Issue247_RouteRedundantNodes.scala
@@ -0,0 +1,23 @@
+package kpn.server.analyzer.engine.analysis.caseStudies
+
+import kpn.api.custom.Tags
+import kpn.core.util.UnitTest
+
+class Issue247_RouteRedundantNodes extends UnitTest {
+
+  test("redundant nodes") {
+    // status 2021-12-20T21:00:00Z
+    val route = CaseStudy.routeAnalysis("13569497").route
+    route.facts shouldBe empty
+    route.summary.name should equal("?-?")
+    route.tags.shouldMatchTo(
+      Tags.from(
+        "note" -> "? - ?",
+        "network" -> "lwn",
+        "network:type" -> "node_network",
+        "route" -> "hiking",
+        "type" -> "route"
+      )
+    )
+  }
+}
