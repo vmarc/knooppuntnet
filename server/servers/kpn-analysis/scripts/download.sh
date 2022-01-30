629acca37aa9ac74fd42263461629eafbe65c77c
vmarc <marc@vmarc.be>
2022-01-30 11:18:03 +0100

    osm data download and merge

---
+++ server/servers/kpn-analysis/scripts/download.sh
@@ -0,0 +1,7 @@
+wget http://download.geofabrik.de/europe/belgium-latest.osm.pbf
+wget http://download.geofabrik.de/europe/netherlands-latest.osm.pbf
+wget http://download.geofabrik.de/europe/germany-latest.osm.pbf
+wget http://download.geofabrik.de/europe/france-latest.osm.pbf
+wget http://download.geofabrik.de/europe/austria-latest.osm.pbf
+wget http://download.geofabrik.de/europe/spain-latest.osm.pbf
+wget http://download.geofabrik.de/europe/luxembourg-latest.osm.pbf
