86959c0804ec29b12b38e3c6bfbdef75b653b76e
vmarc <marc@vmarc.be>
2022-09-11 18:27:19 +0200

    mail server script

---
+++ server/servers/kpn-web/scripts/server-mail.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+nohup /kpn/java/bin/java \
+  -Dname=server-mail \
+  -Dlog4j.configurationFile=/kpn/scripts/conf/server-mail-log.xml \
+  -Xms512M \
+  -Xmx2G \
+  -Xss128M \
+  -Dcom.sun.management.jmxremote.port=5103 \
+  -Dcom.sun.management.jmxremote.authenticate=false \
+  -Dcom.sun.management.jmxremote.ssl=false \
+  -jar /kpn/bin/dist/server-4.0.8.jar \
+  --spring.config.location=classpath:application.properties,file:/kpn/conf/server-mail.properties >> /kpn/logs/server-mail-stdout.log 2>&1 &
