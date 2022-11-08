511203eb7d0ed8f3f4adea9bede521215e03fa8b
vmarc <marc@vmarc.be>
2022-11-09 00:26:57 +0100

    #270 use correct api call parameter (existing route name) to update route

--- client/src/app/monitor/store/monitor.effects.ts
+++ client/src/app/monitor/store/monitor.effects.ts
@@ -218,7 +218,7 @@ export class MonitorEffects {
           );
         }
         return this.monitorService
-          .routeUpdate(groupName, properties.name, properties)
+          .routeUpdate(groupName, parameters.initialProperties.name, properties)
           .pipe(
             map((response) => {
               if (
