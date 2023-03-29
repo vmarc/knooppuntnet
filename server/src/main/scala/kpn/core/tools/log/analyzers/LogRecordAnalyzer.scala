916273f04fdb909269af9f3174b4f398fc979619
vmarc <marc@vmarc.be>
2023-03-29 15:13:18 +0200

    remove nginx log analysis logic

--- server/src/main/scala/kpn/core/tools/log/analyzers/LogRecordAnalyzer.scala
+++
@@ -1,8 +0,0 @@
-package kpn.core.tools.log.analyzers
-
-import kpn.core.tools.log.LogAnalysisContext
-import kpn.core.tools.log.LogRecord
-
-trait LogRecordAnalyzer {
-  def analyze(record: LogRecord, context: LogAnalysisContext): LogAnalysisContext
-}
