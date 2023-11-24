2952965013564cdbc2fdcbab435e840c21bbb7b3
vmarc <marc@vmarc.be>
2023-11-24 15:32:33 +0100

    oauth2-spike-backend serve clientId

--- oauth2-spike/backend/backend/src/main/scala/kpn/backend/AuthenticationController.scala
+++ oauth2-spike/backend/backend/src/main/scala/kpn/backend/AuthenticationController.scala
@@ -4,9 +4,20 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class AuthenticationController {
+class AuthenticationController(oauthClientId: String) {
+
   @GetMapping(value = Array("/api/hello"))
   def hello(): String = {
     "hello"
+  }
+
+  @GetMapping(value = Array("/api/client-id"))
+  def clientId(): String = {
+    oauthClientId
+  }
+
+  @GetMapping(value = Array("/api/user"))
+  def user(): String = {
+    "user"
   }
 }
---
+++ oauth2-spike/backend/backend/src/main/scala/kpn/backend/BackendConfiguration.scala
@@ -0,0 +1,13 @@
+package kpn.backend
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class BackendConfiguration {
+  @Bean
+  def oauthClientId(@Value("${oauth.client-id}") value: String): String = {
+    value
+  }
+}
