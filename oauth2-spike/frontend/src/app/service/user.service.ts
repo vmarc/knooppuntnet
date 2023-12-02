e0c346e58507015b9f8cad7aea97786fbca7febc
vmarc <marc@vmarc.be>
2023-12-02 10:23:55 +0100

    #333 oauth2-spike error handling

--- oauth2-spike/frontend/src/app/service/user.service.ts
+++ oauth2-spike/frontend/src/app/service/user.service.ts
@@ -1,30 +1,37 @@
 import { DOCUMENT } from '@angular/common';
 import { HttpParams } from '@angular/common/http';
 import { HttpClient } from '@angular/common/http';
+import { OnDestroy } from '@angular/core';
 import { inject } from '@angular/core';
 import { signal } from '@angular/core';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { OAuthSuccessEvent } from 'angular-oauth2-oidc';
 import { OAuthService } from 'angular-oauth2-oidc';
-import { map } from "rxjs";
-import { Observable } from "rxjs";
+import { map } from 'rxjs';
+import { Observable } from 'rxjs';
 import { tap } from 'rxjs';
 import { BrowserStorageService } from './browser-storage.service';
+import { Subscriptions } from './subscriptions';
 
 @Injectable()
-export class UserService {
+export class UserService implements OnDestroy {
   private readonly http = inject(HttpClient);
   private readonly router = inject(Router);
   private readonly oauthService = inject(OAuthService);
   private readonly browserStorageService = inject(BrowserStorageService);
   private readonly document = inject(DOCUMENT);
 
+  private readonly subscriptions = new Subscriptions();
+  private readonly localStorageUserKey = 'osm-user-name'; // using new key to avoid clash with old login mechanism
+  private readonly oldLocalStorageUserKey = 'user';
   private showDebugInformation = true;
   private returnUrl: string | null = null;
 
   private readonly _user = signal<string | null>(null);
   readonly user = this._user.asReadonly();
+  private readonly _error = signal<string | null>(null);
+  readonly error = this._error.asReadonly();
 
   private authConfig = {
     issuer: 'https://www.openstreetmap.org',
@@ -41,6 +48,10 @@ export class UserService {
     this.initUser();
   }
 
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
   navigateToReturnUrl(): void {
     if (this.returnUrl) {
       const url = this.returnUrl;
@@ -50,70 +61,88 @@ export class UserService {
   }
 
   login(): void {
-    this.loadDiscoveryDocumentAndTryLogin().pipe(
-      tap(() => {
-        if (this.returnUrl) {
-          this.debug(`login() initCodeFlow() returnUrl=${this.returnUrl}`);
-          this.oauthService.initCodeFlow(this.returnUrl);
-        }
-      })
-    ).subscribe();
+    this.loadDiscoveryDocumentAndTryLogin()
+      .pipe(
+        tap(() => {
+          if (this.returnUrl) {
+            this.debug(`login() initCodeFlow() returnUrl=${this.returnUrl}`);
+            this.oauthService.initCodeFlow(this.returnUrl);
+          }
+        })
+      )
+      .subscribe({
+        next: (v) => this.debug('login() next', v),
+        error: (e) => this.debug('login() next', e),
+        complete: () => this.debug('login() next completed.'),
+      });
   }
 
   authenticated(): void {
-    this.loadDiscoveryDocumentAndTryLogin().subscribe();
+    this.loadDiscoveryDocumentAndTryLogin().subscribe({
+      next: (v) => this.debug('authenticated() next', v),
+      error: (e) => this.debug('authenticated() error', e),
+      complete: () => this.debug('authenticated() completed.'),
+    });
   }
 
   logout(): void {
     this.http
-    .get('/api/logout', {responseType: 'text'})
-    .pipe(
-      tap(() => {
-        this.updateUser(null);
-        if (this.returnUrl) {
-          const url = this.returnUrl;
-          this.returnUrl = null;
-          this.router.navigateByUrl(url);
-        }
-      })
-    )
-    .subscribe();
+      .get('/api/logout', { responseType: 'text' })
+      .pipe(
+        tap(() => {
+          this.updateUser(null);
+          if (this.returnUrl) {
+            const url = this.returnUrl;
+            this.returnUrl = null;
+            this.debug(`navigateByUrl(${url})`);
+            this.router.navigateByUrl(url);
+          }
+        })
+      )
+      .subscribe({
+        next: (v) => this.debug('logout() next', v),
+        error: (e) => this.debug('logout() error', e),
+        complete: () => this.debug('logout() completed.'),
+      });
   }
 
   loginLinkClicked(): void {
-    this.returnUrl = this.router.url;
+    this.returnUrl = this.router.url; // preserve url to return to after login
     this.router.navigate(['/login']);
   }
 
   logoutLinkClicked(): void {
-    this.returnUrl = this.router.url;
+    this.returnUrl = this.router.url; // preserve url to return to after logout
     this.router.navigate(['/logout']);
   }
 
   private initEventHandling(): void {
-    this.oauthService.events.subscribe((event) => {
-      this.debug('UserService OAuthEvent:', event);
-      if (event instanceof OAuthSuccessEvent) {
-        const successEvent = event as OAuthSuccessEvent;
-        if (successEvent.type === 'token_received') {
-          this.tokenReceived();
+    this.subscriptions.add(
+      this.oauthService.events.subscribe((event) => {
+        this.debug('OAuthEvent:', event);
+        if (event instanceof OAuthSuccessEvent) {
+          const successEvent = event as OAuthSuccessEvent;
+          if (successEvent.type === 'token_received') {
+            this.tokenReceived();
+          }
         }
-      }
-    });
+      })
+    );
   }
 
   private initUser() {
-    const userFromStorage = this.browserStorageService.get('user');
-    // this.updateSentryUser(user);
+    // TODO remove following line after one month or so (only needed short time to clean up old mechanism)
+    this.browserStorageService.remove(this.oldLocalStorageUserKey);
+    const userFromStorage = this.browserStorageService.get(this.localStorageUserKey);
+    // this.updateSentryUser(userFromStorage);
     this._user.set(userFromStorage);
+    this.debug(`user from storage: ${userFromStorage}`);
   }
 
   private loadDiscoveryDocumentAndTryLogin(): Observable<void> {
-    return this.http
-    .get('/api/client-id', {responseType: 'text'})
-    .pipe(
+    return this.http.get('/api/client-id', { responseType: 'text' }).pipe(
       map((clientId) => {
-        console.log(`clientId=${clientId}`);
+        this.debug(`configure clientId: ${clientId}`);
         const authConfigWithClientId = {
           ...this.authConfig,
           clientId: clientId,
@@ -127,12 +156,12 @@ export class UserService {
   private updateUser(user: string | null): void {
     // this.updateSentryUser(user);
     if (user) {
-      this.browserStorageService.set('user', user);
+      this.browserStorageService.set(this.localStorageUserKey, user);
       // Sentry.setUser({
       //   _id: user,
       // });
     } else {
-      this.browserStorageService.remove('user');
+      this.browserStorageService.remove(this.localStorageUserKey);
       // Sentry.setUser(null);
     }
     this._user.set(user);
@@ -140,22 +169,27 @@ export class UserService {
 
   private tokenReceived(): void {
     const accessToken = this.oauthService.getAccessToken();
+    this.debug(`received accessToken: ${accessToken}`);
     if (accessToken) {
       const params = new HttpParams().set('accessToken', accessToken);
       this.http
-      .get('/api/authenticated', {params: params, responseType: 'text'})
-      .pipe(
-        tap((username) => {
-          this.updateUser(username);
-          this.oauthService.logOut();
-          // TODO handle UNAUTHORIZED
-          if (this.oauthService.state) {
-            const url = decodeURIComponent(this.oauthService.state);
-            this.router.navigateByUrl(url);
-          }
-        })
-      )
-      .subscribe();
+        .get('/api/authenticated', { params: params, responseType: 'text' })
+        .pipe(
+          tap((username) => {
+            this.updateUser(username);
+            this.oauthService.logOut();
+            // TODO handle UNAUTHORIZED
+            if (this.oauthService.state) {
+              const url = decodeURIComponent(this.oauthService.state);
+              this.router.navigateByUrl(url);
+            }
+          })
+        )
+        .subscribe({
+          next: (v) => this.debug('/api/authenticated HTTP response', v),
+          error: (e) => this.debug('/api/authenticated HTTP Error', e),
+          complete: () => this.debug('/api/authenticated HTTP request completed.'),
+        });
     }
   }
 
@@ -182,7 +216,7 @@ export class UserService {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   private debug(message: string, data?: any): void {
     if (this.showDebugInformation) {
-      console.log(message, data);
+      console.log('UserService: ' + message, data);
     }
   }
 }
