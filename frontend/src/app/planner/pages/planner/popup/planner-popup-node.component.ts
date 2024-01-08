408be3d221f2094fbba118b74c03804db81b9521
vmarc <marc@vmarc.be>
2024-01-08 12:27:30 +0100

    handle node not found situation in planner node popup

--- frontend/src/app/planner/pages/planner/popup/planner-popup-node.component.ts
+++ frontend/src/app/planner/pages/planner/popup/planner-popup-node.component.ts
@@ -29,55 +29,57 @@ import { selectPlannerNetworkType } from '../../../store/planner-selectors';
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
     @if (response$ | async; as response) {
-      <h2>
-        <ng-container i18n="@@map.node-popup.title">Node</ng-container>
-        {{ response.result.name }}
-      </h2>
-      <p>
-        <span class="kpn-label" i18n="@@map.node-popup.last-updated">Last updated</span>
-        <kpn-timestamp [timestamp]="response.result.lastUpdated" />
-      </p>
-      <div>
-        @if (response.result.networkReferences.length === 1) {
-          <span class="kpn-label" i18n="@@map.node-popup.network">Network</span>
-        }
-        @if (response.result.networkReferences.length !== 1) {
-          <span class="kpn-label" i18n="@@map.node-popup.networks">Networks</span>
-        }
-        @if (response.result.networkReferences.length === 0) {
-          <span i18n="@@map.node-popup.no-networks">None</span>
-        }
-        @for (ref of response.result.networkReferences; track ref) {
-          <div class="reference">
-            <a [routerLink]="'/analysis/network/' + ref.id">{{ ref.name }}</a>
-          </div>
-        }
-      </div>
-      @if (response.result.routeReferences.length > 0) {
+      @if (response.result) {
+        <h2>
+          <ng-container i18n="@@map.node-popup.title">Node</ng-container>
+          {{ response.result.name }}
+        </h2>
+        <p>
+          <span class="kpn-label" i18n="@@map.node-popup.last-updated">Last updated</span>
+          <kpn-timestamp [timestamp]="response.result.lastUpdated" />
+        </p>
         <div>
-          <span class="kpn-label" i18n="@@map.node-popup.routes">Routes</span>
-          @if (response.result.routeReferences.length === 0) {
-            <span i18n="@@map.node-popup.routes.none">None</span>
+          @if (response.result.networkReferences.length === 1) {
+            <span class="kpn-label" i18n="@@map.node-popup.network">Network</span>
           }
-          @for (ref of response.result.routeReferences; track ref) {
+          @if (response.result.networkReferences.length !== 1) {
+            <span class="kpn-label" i18n="@@map.node-popup.networks">Networks</span>
+          }
+          @if (response.result.networkReferences.length === 0) {
+            <span i18n="@@map.node-popup.no-networks">None</span>
+          }
+          @for (ref of response.result.networkReferences; track ref) {
             <div class="reference">
-              <kpn-link-route
-                [routeId]="ref.id"
-                [routeName]="ref.name"
-                [networkType]="ref.networkType"
-              />
+              <a [routerLink]="'/analysis/network/' + ref.id">{{ ref.name }}</a>
             </div>
           }
         </div>
+        @if (response.result.routeReferences.length > 0) {
+          <div>
+            <span class="kpn-label" i18n="@@map.node-popup.routes">Routes</span>
+            @if (response.result.routeReferences.length === 0) {
+              <span i18n="@@map.node-popup.routes.none">None</span>
+            }
+            @for (ref of response.result.routeReferences; track ref) {
+              <div class="reference">
+                <kpn-link-route
+                  [routeId]="ref.id"
+                  [routeName]="ref.name"
+                  [networkType]="ref.networkType"
+                />
+              </div>
+            }
+          </div>
+        }
+        <div class="more-details">
+          <a
+            [routerLink]="'/analysis/node/' + response.result.id"
+            i18n="@@map.node-popup.more-details"
+          >
+            More details
+          </a>
+        </div>
       }
-      <div class="more-details">
-        <a
-          [routerLink]="'/analysis/node/' + response.result.id"
-          i18n="@@map.node-popup.more-details"
-        >
-          More details
-        </a>
-      </div>
     }
   `,
   styles: `
@@ -107,8 +109,9 @@ export class PlannerPopupNodeComponent implements OnInit, OnDestroy {
     this.response$ = this.mapService.nodeClicked$.pipe(
       filter((nodeClick) => nodeClick !== null),
       combineLatestWith(this.store.select(selectPlannerNetworkType)),
-      switchMap(([nodeClick, networkType]) =>
-        this.apiService.mapNodeDetail(networkType, +nodeClick.node.node.nodeId).pipe(
+      switchMap(([nodeClick, networkType]) => {
+        const nodeId = +nodeClick.node.node.nodeId;
+        return this.apiService.mapNodeDetail(networkType, nodeId).pipe(
           tap((response) => {
             if (response.result) {
               const coordinate = OlUtil.toCoordinate(
@@ -118,8 +121,8 @@ export class PlannerPopupNodeComponent implements OnInit, OnDestroy {
               this.openPopup(coordinate);
             }
           })
-        )
-      )
+        );
+      })
     );
 
     this.subscriptions.add(
