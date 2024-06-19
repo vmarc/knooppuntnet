caa249d7f412f58a77627c30a0b7793ba9d15cc9
vmarc <marc@vmarc.be>
2024-06-19 13:47:54 +0200

    release 5.0.1

--- server/servers/kpn-analysis/scripts/dispatcher.sh
+++ server/servers/kpn-analysis/scripts/dispatcher.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-nohup /kpn/overpass/bin/dispatcher --osm-base --attic --db-dir=/kpn/database --space=1000000000000 --time=1000000000 --rate-limit=0 >> /kpn/logs/dispatcher-stdout.log 2>&1 &
+nohup $KPN_ROOT/overpass/bin/dispatcher --osm-base --attic --db-dir=$KPN_ROOT/database --space=1000000000000 --time=1000000000 --rate-limit=0 >> $KPN_ROOT/logs/dispatcher-stdout.log 2>&1 &
