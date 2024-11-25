7496e5baae6e7ee66e626311433f7a40d66bfc6f
vmarc <marc@vmarc.be>
2024-11-25 09:14:00 +0100

    eliminate com.wdtinc vector tile encoder/decoder library

--- server/src/main/scala/kpn/core/tools/tile/TileViewTool.scala
+++ server/src/main/scala/kpn/core/tools/tile/TileViewTool.scala
@@ -1,10 +1,8 @@
 package kpn.core.tools.tile
 
-import com.wdtinc.mapbox_vector_tile.adapt.jts.MvtReader
-import com.wdtinc.mapbox_vector_tile.adapt.jts.TagKeyValueMapConverter
-import com.wdtinc.mapbox_vector_tile.adapt.jts.model.JtsLayer
-import com.wdtinc.mapbox_vector_tile.adapt.jts.model.JtsMvt
-import org.locationtech.jts.geom.GeometryFactory
+import no.ecc.vectortile.VectorTileDecoder
+import org.apache.commons.io.FileUtils
+import org.apache.commons.io.IOUtils
 import org.locationtech.jts.geom.LineString
 
 import java.io.File
@@ -14,59 +12,49 @@ import java.util.zip.GZIPInputStream
 
 object TileViewTool {
 
-  private val geometryFactory = new GeometryFactory()
-
   def main(args: Array[String]): Unit = {
-
-    new TileViewTool().print("/Users/marc/kpn/tiles/hiking/9/268/150.mvt")
-    //new TileViewTool().print("/Users/marc/kpn/tiles/tmp/hiking/14/8600/5709.mvt") // Calmeyn
-    //    new TileViewTool().print("/Users/marc/kpn/tiles/hiking/13/4197/2726.mvt") // Calmeyn
-    //    new TileViewTool().print("/Users/marc/kpn/tiles/hiking/11/1049/681.mvt") // Calmeyn
+    new TileViewTool().print("/Users/marc/kpn/tiles/hiking/6/31/21.mvt")
+    new TileViewTool().print("/Users/marc/kpn/tiles/hiking/13/4197/2724.mvt")
     println("Done")
   }
 }
 
 class TileViewTool {
 
-  import TileViewTool.geometryFactory
-
   def print(filename: String): Unit = {
     println(filename)
-    val tile = load(filename)
-    tile.getLayers.forEach(printLayer)
+    val features = load(filename)
+    features.forEach(feature => printFeature(feature))
   }
 
-  private def printLayer(layer: JtsLayer): Unit = {
-    println(s"layer ${layer.getName}, extent=${layer.getExtent}, geometries=${layer.getGeometries.size}")
-    layer.getGeometries.forEach { geometry =>
-      val userData = geometry.getUserData
-      println("  " + userData)
-
-      geometry match {
-        case lineString: LineString =>
-          val message = lineString.getCoordinates.map(coordinate => s"[${coordinate.x},${coordinate.x}]").mkString((","))
-          println(s"    geometry $message")
-        case _ => println("    geometry not a LineString")
-      }
-    }
+  private def printFeature(feature: VectorTileDecoder.Feature): Unit = {
+    val id = feature.getId
+    val layer = feature.getLayerName
+    val extent = feature.getExtent
+    val attributes = feature.getAttributes
+    val geometry = feature.getGeometry.asInstanceOf[LineString]
+    val coordinates = geometry.getCoordinates().map(coordinate => s"[${coordinate.x},${coordinate.x}]").mkString((","))
+    println(s"  id=$id, layer=$layer, extent=$extent, attributes=$attributes")
+    println(s"    $coordinates")
   }
 
-  def load(filename: String): JtsMvt = {
-    if (filename.endsWith(".pbf")) {
+  def load(filename: String): VectorTileDecoder.FeatureIterable = {
+    val bytes = if (filename.endsWith(".pbf")) {
       loadPbf(filename)
     }
     else {
       loadMvt(filename)
     }
+    new VectorTileDecoder().decode(bytes)
   }
 
-  private def loadMvt(filename: String): JtsMvt = {
-    MvtReader.loadMvt(new File(filename), geometryFactory, new TagKeyValueMapConverter())
+  private def loadMvt(filename: String): Array[Byte] = {
+    FileUtils.readFileToByteArray(new File(filename))
   }
 
-  private def loadPbf(filename: String): JtsMvt = {
+  private def loadPbf(filename: String): Array[Byte] = {
     val gzipped: InputStream = new FileInputStream(filename)
     val ungzipped: InputStream = new GZIPInputStream(gzipped)
-    MvtReader.loadMvt(ungzipped, geometryFactory, new TagKeyValueMapConverter())
+    IOUtils.toByteArray(ungzipped)
   }
 }
