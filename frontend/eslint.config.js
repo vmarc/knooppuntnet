af4d6247b6d41a84c2e6d26dde683664b4decd31
vmarc <marc@vmarc.be>
2025-02-08 11:30:48 +0100

    improve app structure - reintroduce sheriff

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,6 +2,7 @@
 const eslint = require("@eslint/js");
 const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
+const sheriff = require("@softarc/eslint-plugin-sheriff");
 
 module.exports = tseslint.config(
   {
@@ -11,6 +12,7 @@ module.exports = tseslint.config(
       ...tseslint.configs.recommended,
       ...tseslint.configs.stylistic,
       ...angular.configs.tsRecommended,
+      sheriff.configs.all,
     ],
     processor: angular.processInlineTemplates,
     rules: {
