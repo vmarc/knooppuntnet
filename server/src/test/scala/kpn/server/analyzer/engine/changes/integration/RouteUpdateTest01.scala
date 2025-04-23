0539a08594f7c3a09dbbcc75afe78ef2b33295be
vmarc <marc@vmarc.be>
2025-04-23 20:22:47 +0200

    refactor analysis - RouteUpdateTest01

--- server/src/test/scala/kpn/server/analyzer/engine/changes/integration/RouteUpdateTest01.scala
+++ server/src/test/scala/kpn/server/analyzer/engine/changes/integration/RouteUpdateTest01.scala
@@ -63,87 +63,54 @@ class RouteUpdateTest01 extends IntegrationTest {
   }
 
   private def assertRouteChange(): Unit = {
-    pendingRedesignPrio1()
     assertEqual(
       findRouteChangeById("123:1:11"),
       newRouteChange(
         newChangeKey(elementId = 11),
         ChangeType.Update,
         "01-02",
-        before = None,
-        //        Some(
-        //          newRouteData(
-        //            Some(Country.nl),
-        //            routeType.hiking,
-        //            relation = newRawRelation(
-        //              11,
-        //              members = Seq(
-        //                RawMember("way", 101, None)
-        //              ),
-        //              tags = Tags.from(
-        //                "network" -> "rwn",
-        //                "type" -> "route",
-        //                "route" -> "foot",
-        //                "ref" -> "01-02",
-        //                "network:type" -> "node_network",
-        //                "key" -> "value1" // <--
-        //              )
-        //            ),
-        //            name = "01-02",
-        //            networkNodes = Seq(
-        //              newNodeWithName(1001, "01"),
-        //              newNodeWithName(1002, "02")
-        //            ),
-        //            nodes = Seq(
-        //              newNodeWithName(1001, "01"),
-        //              newNodeWithName(1002, "02")
-        //            ),
-        //            ways = Seq(
-        //              newRawWay(
-        //                101,
-        //                nodeIds = Vector(1001, 1002),
-        //                tags = Tags.from("highway" -> "unclassified")
-        //              )
-        //            )
-        //          )
-        //        ),
-        after = None,
-        //        Some(
-        //          newRouteData(
-        //            Some(Country.nl),
-        //            routeType.hiking,
-        //            relation = newRawRelation(
-        //              11,
-        //              members = Seq(
-        //                RawMember("way", 101, None)
-        //              ),
-        //              tags = Tags.from(
-        //                "network" -> "rwn",
-        //                "type" -> "route",
-        //                "route" -> "foot",
-        //                "ref" -> "01-02",
-        //                "network:type" -> "node_network",
-        //                "key" -> "value2" // <--
-        //              )
-        //            ),
-        //            name = "01-02",
-        //            networkNodes = Seq(
-        //              newNodeWithName(1001, "01"),
-        //              newNodeWithName(1002, "02")
-        //            ),
-        //            nodes = Seq(
-        //              newNodeWithName(1001, "01"),
-        //              newNodeWithName(1002, "02")
-        //            ),
-        //            ways = Seq(
-        //              newRawWay(
-        //                101,
-        //                nodeIds = Vector(1001, 1002),
-        //                tags = Tags.from("highway" -> "unclassified")
-        //              )
-        //            )
-        //          )
-        //        ),
+        before = Some(
+          newRouteData(
+            relationId = 11,
+            meta = newMetaData(changeSetId = 1),
+            countries = Seq(Country.nl),
+            routeTypes = Seq(RouteType.hiking),
+            name = "01-02",
+            networkNodes = Seq(
+              newRouteNode(1001, "01"),
+              newRouteNode(1002, "02")
+            ),
+            tags = Tags.from(
+              "network" -> "rwn",
+              "type" -> "route",
+              "route" -> "foot",
+              "ref" -> "01-02",
+              "network:type" -> "node_network",
+              "key" -> "value1" // <--
+            ),
+          )
+        ),
+        after = Some(
+          newRouteData(
+            relationId = 11,
+            meta = newMetaData(changeSetId = 1),
+            countries = Seq(Country.nl),
+            routeTypes = Seq(RouteType.hiking),
+            name = "01-02",
+            networkNodes = Seq(
+              newRouteNode(1001, "01"),
+              newRouteNode(1002, "02")
+            ),
+            tags = Tags.from(
+              "network" -> "rwn",
+              "type" -> "route",
+              "route" -> "foot",
+              "ref" -> "01-02",
+              "network:type" -> "node_network",
+              "key" -> "value2" // <--
+            ),
+          )
+        ),
         diffs = RouteDiff(
           tagDiffs = Some(
             TagDiffs(
