bb936007e19155b939dcc37a602737c02c042285
vmarc <marc@vmarc.be>
2025-04-26 09:45:57 +0200

    refactor analysis - ChangeSaver

--- server/src/main/scala/kpn/server/analyzer/engine/changes/ChangeSaver.scala
+++ server/src/main/scala/kpn/server/analyzer/engine/changes/ChangeSaver.scala
@@ -1,5 +1,9 @@
 package kpn.server.analyzer.engine.changes
 
+import kpn.api.common.changes.details.NodeChange
+import kpn.api.common.changes.details.RouteChange
+import kpn.server.analyzer.engine.changes.data.ChangeSetChanges
+import kpn.server.analyzer.engine.changes.network.NetworkChange
 import kpn.server.repository.ChangeSetRepository
 import kpn.server.repository.NetworkInfoRepository
 import org.springframework.stereotype.Component
@@ -9,25 +13,34 @@ class ChangeSaver(
   changeSetRepository: ChangeSetRepository,
   networkInfoRepository: NetworkInfoRepository
 ) {
-
   def save(context: ChangeSetContext): Unit = {
-
-    if (context.changes.nonEmpty) {
-
-      context.changes.networkChanges.foreach { networkChange =>
-        changeSetRepository.saveNetworkChange(networkChange)
-      }
-
-      context.changes.routeChanges.foreach { routeChange =>
-        changeSetRepository.saveRouteChange(routeChange)
-      }
-
-      context.changes.nodeChanges.foreach { nodeChange =>
-        changeSetRepository.saveNodeChange(nodeChange)
-      }
-
-      val changeSetSummary = new ChangeSetSummaryBuilder().build(context)
-      changeSetRepository.saveChangeSetSummary(changeSetSummary)
+    if (context.changes.isEmpty) {
+      return
     }
+    saveAllChanges(context.changes)
+    saveChangeSummary(context)
+  }
+
+  private def saveAllChanges(changes: ChangeSetChanges): Unit = {
+    saveNetworkChanges(changes.networkChanges)
+    saveRouteChanges(changes.routeChanges)
+    saveNodeChanges(changes.nodeChanges)
+  }
+
+  private def saveNetworkChanges(changes: Seq[NetworkChange]): Unit = {
+    changes.foreach(changeSetRepository.saveNetworkChange)
+  }
+
+  private def saveRouteChanges(changes: Seq[RouteChange]): Unit = {
+    changes.foreach(changeSetRepository.saveRouteChange)
+  }
+
+  private def saveNodeChanges(changes: Seq[NodeChange]): Unit = {
+    changes.foreach(changeSetRepository.saveNodeChange)
+  }
+
+  private def saveChangeSummary(context: ChangeSetContext): Unit = {
+    val changeSetSummary = new ChangeSetSummaryBuilder().build(context)
+    changeSetRepository.saveChangeSetSummary(changeSetSummary)
   }
 }
