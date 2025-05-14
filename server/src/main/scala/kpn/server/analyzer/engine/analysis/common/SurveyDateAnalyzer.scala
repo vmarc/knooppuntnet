37e1a9d97f17c1295acdeabd196077e63877f589
vmarc <marc@vmarc.be>
2025-05-14 09:24:42 +0200

    refactor analysis - SurveyDateAnalyzer

--- server/src/main/scala/kpn/server/analyzer/engine/analysis/common/SurveyDateAnalyzer.scala
+++ server/src/main/scala/kpn/server/analyzer/engine/analysis/common/SurveyDateAnalyzer.scala
@@ -2,6 +2,8 @@ package kpn.server.analyzer.engine.analysis.common
 
 import kpn.api.common.data.Tagable
 import kpn.api.custom.Day
+import kpn.server.analyzer.engine.analysis.common.SurveyDateAnalyzer.DayPattern
+import kpn.server.analyzer.engine.analysis.common.SurveyDateAnalyzer.MonthPattern
 
 import scala.util.Failure
 import scala.util.Success
@@ -10,8 +12,8 @@ import scala.util.matching.Regex
 
 object SurveyDateAnalyzer {
 
-  private val monthPattern: Regex = """(20\d\d)-(0[1-9]|1[0-2])""".r
-  private val dayPattern: Regex = """(20\d\d)-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])""".r
+  private val MonthPattern: Regex = """(20\d\d)-(0[1-9]|1[0-2])""".r
+  private val DayPattern: Regex = """(20\d\d)-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])""".r
 
   def analyze(tagable: Tagable): Try[Option[Day]] = {
     new SurveyDateAnalyzer(tagable).analyze()
@@ -21,44 +23,29 @@ object SurveyDateAnalyzer {
 class SurveyDateAnalyzer(tagable: Tagable) {
 
   def analyze(): Try[Option[Day]] = {
-    surveyDate match {
-      case None => Success(None)
-      case Some(string) =>
-        string match {
-          case SurveyDateAnalyzer.monthPattern(year, month) =>
-            Success(
-              Some(
-                Day(
-                  year.toInt,
-                  month.toInt
-                )
-              )
-            )
-          case SurveyDateAnalyzer.dayPattern(year, month, day) =>
-            Success(
-              Some(
-                Day(
-                  year.toInt,
-                  month.toInt,
-                  Some(day.toInt)
-                )
-              )
-            )
-          case _ => Failure(null)
-        }
+    surveyDate.map(parse).getOrElse(Success(None))
+  }
+
+  private def parse(string: String): Try[Option[Day]] = {
+    string match {
+      case MonthPattern(year, month) => createDay(year.toInt, month.toInt)
+      case DayPattern(year, month, day) => createDay(year.toInt, month.toInt, Some(day.toInt))
+      case _ => Failure(null)
     }
   }
 
   private def surveyDate: Option[String] = {
-    tagable.tagValue("survey:date") match {
-      case Some(string) => Some(string)
-      case None =>
-        if (tagable.hasTag("source", "survey")) {
-          tagable.tagValue("source:date")
-        }
-        else {
-          None
-        }
+    tagable.tagValue("survey:date") orElse {
+      if (tagable.hasTag("source", "survey")) {
+        tagable.tagValue("source:date")
+      }
+      else {
+        None
+      }
     }
+  }
+
+  private def createDay(year: Int, month: Int, day: Option[Int] = None): Try[Option[Day]] = {
+    Success(Some(Day(year, month, day)))
   }
 }
