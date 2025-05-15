e8a2b55eb6871d86edf6ea7f22244efbaa369089
vmarc <marc@vmarc.be>
2025-05-15 12:01:00 +0200

    refactor analysis - NetworkExtraAnalyzer

--- server/src/main/scala/kpn/server/analyzer/engine/analysis/network/main/analyzers/NetworkExtraAnalyzer.scala
+++ server/src/main/scala/kpn/server/analyzer/engine/analysis/network/main/analyzers/NetworkExtraAnalyzer.scala
@@ -15,69 +15,65 @@ class NetworkExtraAnalyzer(
 
   override def analyze(context: NetworkAnalysisContext): NetworkAnalysisContext = {
     if (context.network.active) {
-      val wayIds = context.network.members.filter(_.memberType == MemberType.Way).map(_.ref).toSet
-      val nodeIds = context.network.members.filter(_.memberType == MemberType.Node).map(_.ref).toSet
-      val relationIds = context.network.members.filter(_.memberType == MemberType.Relation).map(_.ref).toSet
-
-      val networkNodeIds = context.nodeDetails.map(_.id).toSet
-      val networkRouteIds = context.routeDetails.map(_.id).toSet
-
-      val extraWayIds = wayIds.toSeq.sorted
-      val extraNodeIds = (nodeIds -- networkNodeIds).toSeq.sorted
-      val extraRelationIds = (relationIds -- networkRouteIds).toSeq.sorted
-
-      val extraNodes: Seq[RawNode] = if (extraNodeIds.nonEmpty) {
-        overpassRepository.nodes(context.analysisTimestamp, extraNodeIds)
-      }
-      else {
-        Seq.empty
-      }
-
-      val filteredExtraNodeIds = extraNodeIds.filter { nodeId =>
-        extraNodes.find(_.id == nodeId) match {
-          case Some(nodeDetail) => !isNodeAllowedInNetworkRelation(nodeDetail)
-          case None => true
-        }
-      }
-
-      val facts: Seq[NetworkFact] = Seq(
-        networkFact(Fact.NetworkExtraMemberWay, "way", extraWayIds),
-        networkFact(Fact.NetworkExtraMemberNode, "node", filteredExtraNodeIds),
-        networkFact(Fact.NetworkExtraMemberRelation, "relation", extraRelationIds),
-      ).flatten
-
-      //  context.networkDoc.networkFacts.integrityCheckFailed.toSeq.map { integrityCheckFailed =>
-      //    NetworkFact(
-      //      Fact.IntegrityCheckFailed.name,
-      //      checks = Some(
-      //        integrityCheckFailed.checks.map { c =>
-      //          Check(c.nodeId, c.nodeName, c.actual, c.expected)
-      //        }
-      //      )
-      //    )
-      //  },
-      //  context.networkDoc.networkFacts.nameMissing.toSeq.map { x =>
-      //    NetworkFact(Fact.NameMissing.name)
-      //  }
-
-      //    val networkInfoFacts = context.networkFacts.map(f => NetworkFact(f.name))
-      //    networkInfoFacts ++ networkInfoDocFacts
-
-      context.copy(
-        _extraNodeIds = Some(filteredExtraNodeIds),
-        _extraWayIds = Some(extraWayIds),
-        _extraRelationIds = Some(extraRelationIds),
-        _networkFacts = Some(context.networkFacts ++ facts)
-      )
+      analyzeNetwork(context)
     }
     else {
-      context.copy(
-        _extraNodeIds = Some(Seq.empty),
-        _extraWayIds = Some(Seq.empty),
-        _extraRelationIds = Some(Seq.empty),
-        _networkFacts = Some(Seq.empty)
-      )
+      handleInactiveNetwork(context)
     }
+  }
+
+  private def analyzeNetwork(context: NetworkAnalysisContext) = {
+
+    val wayIds = memberIds(context, MemberType.Way)
+    val nodeIds = memberIds(context, MemberType.Node)
+    val relationIds = memberIds(context, MemberType.Relation)
+
+    val networkNodeIds = context.nodeDetails.map(_.id).toSet
+    val networkRouteIds = context.routeDetails.map(_.id).toSet
+
+    val extraWayIds = wayIds.toSeq.sorted
+    val extraNodeIds = (nodeIds -- networkNodeIds).toSeq.sorted
+    val extraRelationIds = (relationIds -- networkRouteIds).toSeq.sorted
+
+    val extraNodes: Seq[RawNode] = if (extraNodeIds.nonEmpty) {
+      overpassRepository.nodes(context.analysisTimestamp, extraNodeIds)
+    }
+    else {
+      Seq.empty
+    }
+
+    val filteredExtraNodeIds = extraNodeIds.filter { nodeId =>
+      extraNodes.find(_.id == nodeId) match {
+        case Some(nodeDetail) => !isNodeAllowedInNetworkRelation(nodeDetail)
+        case None => true
+      }
+    }
+
+    val networkFacts = Seq(
+      networkFact(Fact.NetworkExtraMemberWay, "way", extraWayIds),
+      networkFact(Fact.NetworkExtraMemberNode, "node", filteredExtraNodeIds),
+      networkFact(Fact.NetworkExtraMemberRelation, "relation", extraRelationIds),
+    ).flatten
+
+    context.copy(
+      _extraNodeIds = Some(filteredExtraNodeIds),
+      _extraWayIds = Some(extraWayIds),
+      _extraRelationIds = Some(extraRelationIds),
+      _networkFacts = Some(context.networkFacts ++ networkFacts)
+    )
+  }
+
+  private def handleInactiveNetwork(context: NetworkAnalysisContext) = {
+    context.copy(
+      _extraNodeIds = Some(Seq.empty),
+      _extraWayIds = Some(Seq.empty),
+      _extraRelationIds = Some(Seq.empty),
+      _networkFacts = Some(Seq.empty)
+    )
+  }
+
+  private def memberIds(context: NetworkAnalysisContext, memberType: MemberType): Set[Long] = {
+    context.network.members.filter(_.memberType == memberType).map(_.ref).toSet
   }
 
   private def isNodeAllowedInNetworkRelation(tagable: Tagable): Boolean = {
@@ -86,17 +82,12 @@ class NetworkExtraAnalyzer(
   }
 
   private def networkFact(fact: Fact, elementType: String, ids: Seq[Long]): Option[NetworkFact] = {
-    if (ids.nonEmpty) {
-      Some(
-        NetworkFact(
-          fact,
-          elementType = Some(elementType),
-          elementIds = Some(ids)
-        )
+    Option.when(ids.nonEmpty) {
+      NetworkFact(
+        fact,
+        elementType = Some(elementType),
+        elementIds = Some(ids)
       )
-    }
-    else {
-      None
     }
   }
 }
--- server/src/test/scala/kpn/server/analyzer/engine/analysis/network/main/analyzers/NetworkExtraAnalyzerTest.scala
+++ server/src/test/scala/kpn/server/analyzer/engine/analysis/network/main/analyzers/NetworkExtraAnalyzerTest.scala
@@ -7,7 +7,6 @@ import kpn.api.common.data.MemberType
 import kpn.api.common.data.raw.RawMember
 import kpn.api.custom.Tags
 import kpn.api.custom.Timestamp
-import kpn.core.test.TestData
 import kpn.core.util.UnitTest
 import kpn.server.overpass.OverpassRepository
 import org.scalamock.scalatest.MockFactory
@@ -15,13 +14,15 @@ import org.scalamock.scalatest.MockFactory
 class NetworkExtraAnalyzerTest extends UnitTest with MockFactory with SharedTestObjects {
 
   test("network relation without members") {
-    pendingRedesignPrio2()
 
     val overpassRepository: OverpassRepository = null
-    val analysisTimestamp: Timestamp = Timestamp(2020, 11, 8)
+
     val contextBefore = NetworkAnalysisContext(
       newBaseNetworkDoc(1),
-      analysisTimestamp,
+      defaultTimestamp,
+      _nodeDetails = Some(Seq.empty),
+      _routeDetails = Some(Seq.empty),
+      _networkFacts = Some(Seq.empty),
     )
 
     val contextAfter = new NetworkExtraAnalyzer(overpassRepository).analyze(contextBefore)
@@ -33,21 +34,21 @@ class NetworkExtraAnalyzerTest extends UnitTest with MockFactory with SharedTest
   }
 
   test("networkExtraMemberNode") {
-    pendingRedesignPrio2()
 
     val overpassRepository = stub[OverpassRepository]
     (overpassRepository.nodes _).when(*, *).returns(Seq(newRawNode(1001)))
 
-    val analysisTimestamp: Timestamp = Timestamp(2020, 11, 8)
-    val network = newBaseNetworkDoc(
-      1,
-      members = Seq(
-        RawMember(MemberType.Node, 1001, None)
-      )
-    )
     val contextBefore = NetworkAnalysisContext(
-      network,
-      analysisTimestamp,
+      newBaseNetworkDoc(
+        1,
+        members = Seq(
+          RawMember(MemberType.Node, 1001, None)
+        )
+      ),
+      defaultTimestamp,
+      _nodeDetails = Some(Seq.empty),
+      _routeDetails = Some(Seq.empty),
+      _networkFacts = Some(Seq.empty),
     )
 
     val contextAfter = new NetworkExtraAnalyzer(overpassRepository).analyze(contextBefore)
@@ -73,7 +74,6 @@ class NetworkExtraAnalyzerTest extends UnitTest with MockFactory with SharedTest
   }
 
   test("no fact networkExtraMemberNode when map, guidepost, board or route_marker") {
-    pendingRedesignPrio2()
 
     val node = newRawNode(
       1001
@@ -112,22 +112,31 @@ class NetworkExtraAnalyzerTest extends UnitTest with MockFactory with SharedTest
     )
 
     val overpassRepository = stub[OverpassRepository]
-    (overpassRepository.nodes _).when(*, *).returns(Seq(node, mapNode, guidepostNode, boardNode, routeMarkerNode))
-
-    val analysisTimestamp: Timestamp = Timestamp(2020, 11, 8)
-    val network = newBaseNetworkDoc(
-      1,
-      members = Seq(
-        RawMember(MemberType.Node, 1001, None),
-        RawMember(MemberType.Node, 1002, None),
-        RawMember(MemberType.Node, 1003, None),
-        RawMember(MemberType.Node, 1004, None),
-        RawMember(MemberType.Node, 1005, None)
+    (overpassRepository.nodes _).when(*, *).returns(
+      Seq(
+        node,
+        mapNode,
+        guidepostNode,
+        boardNode,
+        routeMarkerNode
       )
     )
+
     val contextBefore = NetworkAnalysisContext(
-      network,
-      analysisTimestamp,
+      newBaseNetworkDoc(
+        1,
+        members = Seq(
+          RawMember(MemberType.Node, 1001, None),
+          RawMember(MemberType.Node, 1002, None),
+          RawMember(MemberType.Node, 1003, None),
+          RawMember(MemberType.Node, 1004, None),
+          RawMember(MemberType.Node, 1005, None)
+        )
+      ),
+      defaultTimestamp,
+      _nodeDetails = Some(Seq.empty),
+      _routeDetails = Some(Seq.empty),
+      _networkFacts = Some(Seq.empty),
     )
 
     val contextAfter = new NetworkExtraAnalyzer(overpassRepository).analyze(contextBefore)
@@ -152,203 +161,76 @@ class NetworkExtraAnalyzerTest extends UnitTest with MockFactory with SharedTest
     )
   }
 
-  test("networkExtraMemberNode - not generated when proposed node in non-proposed network") {
-
-    pendingRedesign()
-
-    val node1001 = newRawNode(1002, tags = Tags.from("network:type" -> "node_network", "rwn_ref" -> "01"))
-    val node1002 = newRawNode(1002, tags = Tags.from("network:type" -> "node_network", "proposed:rwn_ref" -> "02"))
-    val node1003 = newRawNode(1003, tags = Tags.from("network:type" -> "node_network", "proposed:rwn_name" -> "03"))
-    val node1004 = newRawNode(1004, tags = Tags.from("network:type" -> "node_network", "rwn_ref" -> "04", "state" -> "proposed"))
-    val node1005 = newRawNode(1005, tags = Tags.from("network:type" -> "node_network", "rwn_name" -> "05", "state" -> "proposed"))
-
-    //    val d = new TestData() {
-    //      networkNode(1001, "01")
-    //      node(1002, Tags.from("network:type" -> "node_network", "proposed:rwn_ref" -> "02"))
-    //      node(1003, Tags.from("network:type" -> "node_network", "proposed:rwn_name" -> "03"))
-    //      node(1004, Tags.from("network:type" -> "node_network", "rwn_ref" -> "04", "state" -> "proposed"))
-    //      node(1005, Tags.from("network:type" -> "node_network", "rwn_name" -> "05", "state" -> "proposed"))
-    //      relation(
-    //        1,
-    //        Seq(
-    //          newMember(MemberType.node, 1001),
-    //          newMember(MemberType.node, 1002),
-    //          newMember(MemberType.node, 1003),
-    //          newMember(MemberType.node, 1004),
-    //          newMember(MemberType.node, 1005)
-    //        )
-    //      )
-    //    }
-
-    val analysisTimestamp: Timestamp = Timestamp(2020, 11, 8)
-    val network = newBaseNetworkDoc(
-      1,
-      members = Seq(
-        RawMember(MemberType.Node, 1001, None),
-        RawMember(MemberType.Node, 1002, None),
-        RawMember(MemberType.Node, 1003, None),
-        RawMember(MemberType.Node, 1004, None),
-        RawMember(MemberType.Node, 1005, None)
-      )
-    )
-    val contextBefore = NetworkAnalysisContext(
-      network,
-      analysisTimestamp,
-    )
-
-    val rawNodes = Seq(
-      node1002,
-      node1003,
-      node1004,
-      node1005
-    )
+  test("networkExtraMemberWay relation without members") {
 
     val overpassRepository = stub[OverpassRepository]
-    (overpassRepository.nodes _).when(*, *).returns(rawNodes)
+    (overpassRepository.nodes _).when(*, *).returns(Seq.empty)
+
+    val contextBefore = NetworkAnalysisContext(
+      newBaseNetworkDoc(
+        1,
+        members = Seq(
+          RawMember(MemberType.Way, 11, None),
+        )
+      ),
+      Timestamp(2020, 11, 8),
+      _nodeDetails = Some(Seq.empty),
+      _routeDetails = Some(Seq.empty),
+      _networkFacts = Some(Seq.empty),
+    )
 
     val contextAfter = new NetworkExtraAnalyzer(overpassRepository).analyze(contextBefore)
 
-    assertEqual(contextAfter.extraNodeIds, Seq.empty)
-    assertEqual(contextAfter.extraWayIds, Seq.empty)
-    assertEqual(contextAfter.extraRelationIds, Seq.empty)
-    assertEqual(contextAfter.facts, Seq.empty)
-    assertEqual(contextAfter.networkFacts, Seq.empty)
+    assertEqual(contextAfter.extraWayIds, Seq(11))
 
-    //    val network = analyze(d)
-    //    network.facts.networkExtraMemberNode should equal(None)
-  }
-
-  test("networkExtraMemberNode - not generated when non-proposed node in proposed network") {
-
-    pendingRedesign()
-
-    val d = new TestData() {
-      networkNode(1001, "01")
-      node(1002, Tags.from("network:type" -> "node_network", "proposed:rwn_ref" -> "02"))
-      node(1003, Tags.from("network:type" -> "node_network", "proposed:rwn_name" -> "03"))
-      node(1004, Tags.from("network:type" -> "node_network", "rwn_ref" -> "04", "state" -> "proposed"))
-      node(1005, Tags.from("network:type" -> "node_network", "rwn_name" -> "05", "state" -> "proposed"))
-      relation(
-        1,
-        Seq(
-          newMember(MemberType.Node, 1001),
-          newMember(MemberType.Node, 1002),
-          newMember(MemberType.Node, 1003),
-          newMember(MemberType.Node, 1004),
-          newMember(MemberType.Node, 1005)
-        ),
-        tags = Tags.from("state" -> "proposed")
-      )
-    }
-    //  val network = analyze(d)
-    //  network.facts.networkExtraMemberNode should equal(None)
-  }
-
-  test("networkExtraMemberWay relation without members") {
-
-    pendingRedesign()
-
-    val d = new TestData() {
-      way(1)
-      relation(
-        1,
-        Seq(
-          newMember(MemberType.Way, 1)
+    assertEqual(
+      contextAfter.networkFacts,
+      Seq(
+        NetworkFact(
+          Fact.NetworkExtraMemberWay,
+          Some("way"),
+          Some(List(11)
+          ),
+          None,
+          None
         )
       )
-    }
-
-    //  val network = analyze(d)
-    //  network.facts.networkExtraMemberWay should equal(Some(Seq(NetworkExtraMemberWay(1))))
+    )
   }
 
   test("networkExtraMemberRelation") {
 
-    pendingRedesign()
+    val overpassRepository = stub[OverpassRepository]
+    (overpassRepository.nodes _).when(*, *).returns(Seq.empty)
 
-    val d = new TestData() {
-      relation(10, Seq.empty, newRouteTags("01-02")) // valid route relation
-      relation(20, Seq.empty, Seq.empty) // not a route relation
-      relation(
+    val contextBefore = NetworkAnalysisContext(
+      newBaseNetworkDoc(
         1,
-        Seq(
-          newMember(MemberType.Relation, 10),
-          newMember(MemberType.Relation, 20)
+        members = Seq(
+          RawMember(MemberType.Relation, 2, None),
+        )
+      ),
+      defaultTimestamp,
+      _nodeDetails = Some(Seq.empty),
+      _routeDetails = Some(Seq.empty),
+      _networkFacts = Some(Seq.empty),
+    )
+
+    val contextAfter = new NetworkExtraAnalyzer(overpassRepository).analyze(contextBefore)
+
+    assertEqual(contextAfter.extraRelationIds, Seq(2))
+
+    assertEqual(
+      contextAfter.networkFacts,
+      Seq(
+        NetworkFact(
+          Fact.NetworkExtraMemberRelation,
+          Some("relation"),
+          Some(Seq(2)),
+          None,
+          None
         )
       )
-    }
-
-    //  val network = analyze(d)
-    //  network.facts.networkExtraMemberRelation should equal(Some(Seq(NetworkExtraMemberRelation(20))))
-  }
-
-  test("routes") {
-
-    pendingRedesign()
-
-    val d = new TestData() {
-      relation(10, Seq.empty, newRouteTags("01-03"))
-      relation(20, Seq.empty, newRouteTags("01-02"))
-      relation(30, Seq.empty, newRouteTags("02-03"))
-      relation(
-        1,
-        Seq(
-          newMember(MemberType.Relation, 10, "forward"),
-          newMember(MemberType.Relation, 20, "backward"),
-          newMember(MemberType.Relation, 30)
-        )
-      )
-    }
-
-    //  val routes = analyze(d).routes
-    //  routes.head.routeAnalysis.route.summary.name should equal("01-02")
-    //  routes(1).routeAnalysis.route.summary.name should equal("01-03")
-    //  routes.head.role should equal(Some("backward"))
-    //  routes(1).role should equal(Some("forward"))
-  }
-
-  test("nodes") {
-
-    pendingRedesign()
-
-    val d = new TestData() {
-      node(1001, tags = Tags.from("network:type" -> "node_network", "rwn_ref" -> "01"))
-      node(1002, tags = Tags.from("network:type" -> "node_network", "proposed:rwn_ref" -> "02"))
-      node(1003, tags = Tags.from("network:type" -> "node_network", "rwn_name" -> "Node3"))
-      node(1004, tags = Tags.from("network:type" -> "node_network", "proposed:rwn_name" -> "Node4"))
-      node(1005, tags = Tags.from("network:type" -> "node_network", "rwn_ref" -> "05", "state" -> "proposed"))
-      node(1006, tags = Tags.from("network:type" -> "node_network", "rwn_name" -> "Node6", "state" -> "proposed"))
-      relation(
-        1,
-        Seq(
-          newMember(MemberType.Node, 1001),
-          newMember(MemberType.Node, 1002),
-          newMember(MemberType.Node, 1003),
-          newMember(MemberType.Node, 1004),
-          newMember(MemberType.Node, 1005),
-          newMember(MemberType.Node, 1006)
-        )
-      )
-    }
-
-    //  val nodes = analyze(d).nodes
-    //
-    //  def node(id: Long): NetworkNodeInfo = {
-    //    nodes.find(_.id == id).get
-    //  }
-    //
-    //  node(1001).networkNode.name should equal("01")
-    //  node(1002).networkNode.name should equal("02")
-    //  node(1003).networkNode.name should equal("Node3")
-    //  node(1004).networkNode.name should equal("Node4")
-    //  node(1005).networkNode.name should equal("05")
-    //  node(1006).networkNode.name should equal("Node6")
-    //
-    //  node(1001).proposed should equal(false)
-    //  node(1002).proposed should equal(true)
-    //  node(1003).proposed should equal(false)
-    //  node(1004).proposed should equal(true)
-    //  node(1005).proposed should equal(true)
-    //  node(1006).proposed should equal(true)
+    )
   }
 }
