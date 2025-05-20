223189c83fcce9997680007276342b9f8b620cf6
vmarc <marc@vmarc.be>
2025-05-20 14:23:42 +0200

    refactor analysis - Issue247_RouteRedundantNodes

--- server/src/test/scala/kpn/server/analyzer/engine/analysis/caseStudies/Issue247_RouteRedundantNodes.scala
+++ server/src/test/scala/kpn/server/analyzer/engine/analysis/caseStudies/Issue247_RouteRedundantNodes.scala
@@ -1,16 +1,24 @@
 package kpn.server.analyzer.engine.analysis.caseStudies
 
+import kpn.api.common.Fact
 import kpn.api.custom.Tags
 import kpn.core.util.UnitTest
 
 class Issue247_RouteRedundantNodes extends UnitTest {
 
-  test("redundant nodes") {
-    pendingRedesign()
+  test("no redundant nodes") {
+
     // status 2021-12-20T21:00:00Z
     val route = CaseStudy.baseRouteDoc("13569497")
-    route.facts shouldBe empty
+
+    route.facts shouldNot contain(Fact.RouteRedundantNodes)
+
+    route.nodes.startNode.map(_.alternateName) should equal(Some("?.a"))
+    route.nodes.startTentacleNodes.map(_.alternateName) should equal(Seq("?.b"))
+    route.nodes.endNode should equal(None)
+
     route.summary.name should equal("?-?")
+
     assertEqual(
       route.summary.tags,
       Tags.from(
